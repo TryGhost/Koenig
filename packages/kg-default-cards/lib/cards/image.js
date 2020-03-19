TryGhost/Koenig

commit 56df15ae8e373061e9bb24b3d43f0e38e4f0ceed

Moved `create-card.js`
no issue

- prep for easier move to external repo

packages/kg-default-cards/lib/cards/image.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-cardd');
 
 module.exports = createCard({
     name: 'image',

packages/kg-default-cards/lib/cards/bookmark.js
@@ -17,7 +17,7 @@
 </figure>
  */
 
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 function createElement(dom, elem, classNames = '', attributes = [], text) {
     let element = dom.createElement(elem);

packages/kg-default-cards/lib/cards/card-markdown.js
@@ -1,7 +1,7 @@
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./_markdown');
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 const v1CompatMarkdownCard = markdownCard();
 v1CompatMarkdownCard.name = 'card-markdown';

packages/kg-default-cards/lib/cards/code.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'code',

packages/kg-default-cards/lib/cards/embed.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'embed',

packages/kg-default-cards/lib/cards/gallery.js
@@ -15,7 +15,7 @@
  * </figure>
  */
 
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 const MAX_IMG_PER_ROW = 3;
 

packages/kg-default-cards/lib/cards/hr.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'hr',

packages/kg-default-cards/lib/cards/html.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'html',

commit 0e9c4581e68f165183eb524cf0cd1293ecb8aba0

Fixed typo
no issue

- typo in require introduced in https://github.com/TryGhost/Ghost/commit/56df15ae8e373061e9bb24b3d43f0e38e4f0ceed

packages/kg-default-cards/lib/cards/image.js
@@ -1,4 +1,4 @@
-const createCard = require('./lib/create-cardd');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'image',