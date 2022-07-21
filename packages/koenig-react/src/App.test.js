83801f4692eb0e6f15ae8e8f6bd49fd4b92743ef
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-07-21 14:11:11 +0200

    modified test to check if The Editor renders

--- packages/koenig-react/src/App.test.js
+++ packages/koenig-react/src/App.test.js
@@ -1,8 +1,8 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders The Editor!', () => {
     render(<App />);
-    const linkElement = screen.getByText(/Koenig-React/i);
+    const linkElement = screen.getByText(/The Editor!/i);
     expect(linkElement).toBeInTheDocument();
 });
