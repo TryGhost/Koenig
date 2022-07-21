8d34d6c7391c736b6d9bd0424d9a4e63ac1d9230
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-07-21 10:23:51 +0200

    Exports default as component instead of App

--- packages/koenig-react/src/App.js
+++ packages/koenig-react/src/App.js
@@ -4,6 +4,7 @@ import {Editor, Container, Toolbar} from 'react-mobiledoc-editor';
 function App() {
     return (
         <>
+            <h1 className='text-center'>The Editor</h1>
             <Container>
                 <Toolbar className="flex" />
                 <Editor/>
--- packages/koenig-react/webpack.config.js
+++ packages/koenig-react/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/app.js',
     output: {
         filename: 'koenig-react.min.js',
         path: __dirname + '/dist/umd',
