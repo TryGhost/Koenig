3c84d0e93ca81503d296f53a1c1189b2d7ed93f5
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-08-09 09:49:00 +0200

    Add payload to image card source no issue
    - Gets the image and alt data from payload else replace with static image

--- packages/koenig-react/src/cards/image.js
+++ packages/koenig-react/src/cards/image.js
@@ -2,6 +2,7 @@ import createComponentCard from '../utils/create-component-card';
 import Card from '../components/Card';
 
 const ImageCardComponent = ({...props}) => {
+    const payload = props?.payload;
     return (
         <Card
             className="kg-card-hover"
@@ -13,8 +14,11 @@ const ImageCardComponent = ({...props}) => {
             hasEditMode={true}
             editor={props.editor}
         >
+            <div>
+
+            </div>
             <div className='__mobiledoc-card' contentEditable="false">
-                <img src={props?.src || `https://images.unsplash.com/photo-1524612219806-a2423c90e45e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`} alt={props?.alt || 'image alt description'} />
+                <img src={payload?.src || `https://images.unsplash.com/photo-1524612219806-a2423c90e45e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80`} alt={payload?.alt || 'image alt description'} />
             </div>
         </Card>
     );
