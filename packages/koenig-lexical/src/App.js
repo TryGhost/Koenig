76784c6d8fce069cdd3f4f9cd821c731101a00a4
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-08-18 14:12:33 +0200

    Setup basic lexical instance to start playing around. no issue

--- packages/koenig-lexical/src/App.js
+++ packages/koenig-lexical/src/App.js
@@ -1,10 +1,54 @@
 import './index.css';
+import {LexicalComposer} from '@lexical/react/LexicalComposer';
+import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
+import {HeadingNode, QuoteNode} from '@lexical/rich-text';
+import {ListItemNode, ListNode} from '@lexical/list';
+import {CodeHighlightNode, CodeNode} from '@lexical/code';
+import {AutoLinkNode, LinkNode} from '@lexical/link';
+import {LinkPlugin} from '@lexical/react/LexicalLinkPlugin';
+import {ListPlugin} from '@lexical/react/LexicalListPlugin';
+import {MarkdownShortcutPlugin} from '@lexical/react/LexicalMarkdownShortcutPlugin';
+import {TRANSFORMERS} from '@lexical/markdown';
 
-const KoenigEditor = () => {
-    return (
-        <div data-testid="koenig-editor" className="koenig-react">
-        </div>
-    );
+const editorConfig = {
+    // The editor theme
+    // Handling of errors during update
+    onError(error) {
+        throw error;
+    },
+    // Any custom nodes go here
+    nodes: [
+        HeadingNode,
+        ListNode,
+        ListItemNode,
+        QuoteNode,
+        CodeNode,
+        CodeHighlightNode,
+        AutoLinkNode,
+        LinkNode
+    ]
 };
 
-export default KoenigEditor;
+export default function Editor() {
+    return (
+        <LexicalComposer initialConfig={editorConfig}>
+            <div className="koenig-react">
+
+                <div className="editor-inner">
+                    <RichTextPlugin
+                        contentEditable={<ContentEditable />}
+                        placeholder={<div>Enter some rich text...</div>}
+                    />
+                    <HistoryPlugin />
+                    <AutoFocusPlugin />
+                    <ListPlugin />
+                    <LinkPlugin />
+                    <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+                </div>
+            </div>
+        </LexicalComposer>
+    );
+}
