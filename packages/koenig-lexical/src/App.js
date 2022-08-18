a6c622572bc767fafcc1a266d8421ee842960af2
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-08-18 12:55:44 +0200

    Setup new package for experimenting with Lexical. no issue

---
+++ packages/koenig-lexical/src/App.js
@@ -0,0 +1,10 @@
+import './index.css';
+
+const KoenigEditor = () => {
+    return (
+        <div data-testid="koenig-editor" className="koenig-react">
+        </div>
+    );
+};
+
+export default KoenigEditor;
