e9ea4ef7caa13c5ced72fc0299f12f988fbd0e31
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-19 14:29:34 +0100

    adjusted toolbar positioning to better match existing editor

--- packages/koenig-lexical-playground/src/lib/utils/setFloatingElemPosition.js
+++ packages/koenig-lexical-playground/src/lib/utils/setFloatingElemPosition.js
@@ -1,19 +1,10 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
 const VERTICAL_GAP = 10;
-const HORIZONTAL_OFFSET = 5;
 
 export function setFloatingElemPosition(
     targetRect,
     floatingElem,
     anchorElem,
     verticalGap = VERTICAL_GAP,
-    horizontalOffset = HORIZONTAL_OFFSET,
 ) {
     const scrollerElem = anchorElem.parentElement;
 
@@ -30,14 +21,10 @@ export function setFloatingElemPosition(
     const editorScrollerRect = scrollerElem.getBoundingClientRect();
 
     let top = targetRect.top - floatingElemRect.height - verticalGap;
-    let left = targetRect.left - horizontalOffset;
-
-    if (top < editorScrollerRect.top) {
-        top += floatingElemRect.height + targetRect.height + verticalGap * 2;
-    }
+    let left = targetRect.left + targetRect.width / 2 - floatingElemRect.width / 2;
 
     if (left + floatingElemRect.width > editorScrollerRect.right) {
-        left = editorScrollerRect.right - floatingElemRect.width - horizontalOffset;
+        left = editorScrollerRect.right - floatingElemRect.width;
     }
 
     top -= anchorElementRect.top;
