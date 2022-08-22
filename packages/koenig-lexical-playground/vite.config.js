2669f969d87e1d05b980c01f66a73269ee3226d8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-22 12:05:03 +0100

    bundle JS+CSS in single JS file

--- packages/koenig-lexical-playground/vite.config.js
+++ packages/koenig-lexical-playground/vite.config.js
@@ -2,10 +2,15 @@ import {resolve} from 'path';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
+    plugins: [
+        svgr(),
+        cssInjectedByJs({styleId: 'koenig-lexical-style'}),
+        react()
+    ],
     build: {
         minify: true,
         sourcemap: true,
