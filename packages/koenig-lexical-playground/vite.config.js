3c410a3539ab931cc04d653fa19b5ee13f015a84
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-22 13:44:19 +0100

    use Vite's default CSS injection in UMD build
    - the 3rd party plugin was throwing off the source mapping

--- packages/koenig-lexical-playground/vite.config.js
+++ packages/koenig-lexical-playground/vite.config.js
@@ -2,13 +2,11 @@ import {resolve} from 'path';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import cssInjectedByJs from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svgr(),
-        cssInjectedByJs({styleId: 'koenig-lexical-style'}),
         react()
     ],
     define: {
@@ -17,6 +15,7 @@ export default defineConfig({
     build: {
         minify: true,
         sourcemap: true,
+        cssCodeSplit: true,
         lib: {
             entry: resolve(__dirname, 'src/lib/index.js'),
             name: 'KoenigLexical',
