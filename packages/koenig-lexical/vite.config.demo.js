29da89ae2b0f1c7c03f1c82e0adfcd206fdbd24f
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-19 17:16:16 +0200

    Updated vite config

--- packages/koenig-lexical/vite.config.demo.js
+++ packages/koenig-lexical/vite.config.demo.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/koenig-lexical-demo/',
+  base: '/Koenig/koenig-lexical-demo/',
   build: {
     rollupOptions: {
       input: {
