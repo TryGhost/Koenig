8b09713b14d293f59a261533d48b9f0185901d04
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-19 17:18:55 +0200

    Changed base to `/Koenig/`

--- packages/koenig-lexical/vite.config.demo.js
+++ packages/koenig-lexical/vite.config.demo.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Koenig/koenig-lexical-demo/',
+  base: '/Koenig/',
   build: {
     rollupOptions: {
       input: {
