dd4c26ab4bb8b5aa386c7b15a2e5d3b7f511efcb
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-19 17:30:49 +0200

    Try `/` as base

--- packages/koenig-lexical/vite.config.demo.js
+++ packages/koenig-lexical/vite.config.demo.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Koenig/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
