13d8c05ad10282a60d7565ff0474bcb9d3882508
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-19 17:08:46 +0200

    Added base url to demo config

--- packages/koenig-lexical/vite.config.demo.js
+++ packages/koenig-lexical/vite.config.demo.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/koenig-lexical-demo/',
   build: {
     rollupOptions: {
       input: {
