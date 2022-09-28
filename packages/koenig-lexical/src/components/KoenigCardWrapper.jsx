c76063f8c400456fd4dc61bef7be897ece4220fb
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-28 17:40:13 +0100

    Added initial click-to-select behaviour to `<KoenigCardWrapper>`
    refs https://github.com/TryGhost/Team/issues/1971
    
    - uses Lexical's `updateListener` and `CLICK_COMMAND` to set `isSelected` state when clicked
    - adds green border to the card when selected

--- packages/koenig-lexical/src/components/KoenigCardWrapper.jsx
+++ packages/koenig-lexical/src/components/KoenigCardWrapper.jsx
@@ -1,6 +1,40 @@
-const KoenigCardWrapperComponent = ({children}) => {
+import React from 'react';
+import {mergeRegister} from '@lexical/utils';
+import {useLexicalNodeSelection} from '@lexical/react/useLexicalNodeSelection';
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {$getSelection, $isNodeSelection, CLICK_COMMAND, COMMAND_PRIORITY_LOW} from 'lexical';
+
+const KoenigCardWrapperComponent = ({nodeKey, children}) => {
+    const [editor] = useLexicalComposerContext();
+    const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(nodeKey);
+    const [selection, setSelection] = React.useState(null);
+    const ref = React.useRef(null);
+
+    React.useEffect(() => {
+        return mergeRegister(
+            editor.registerUpdateListener(({editorState}) => {
+                setSelection(editorState.read(() => $getSelection()));
+            }),
+            editor.registerCommand(
+                CLICK_COMMAND,
+                (event) => {
+                    console.log(event.target, ref.current, ref.current.contains(event.target));
+                    (ref.current.contains(event.target)) ? setSelected(true) : clearSelection();
+                    return true;
+                },
+                COMMAND_PRIORITY_LOW
+            )
+        );
+    }, [editor, isSelected, setSelected, clearSelection, nodeKey]);
+
+    const isFocused = $isNodeSelection(selection) && isSelected;
+
     return (
-        <div className="relative caret-grey-800 hover:shadow-[0_0_0_1px] hover:shadow-green" data-kg-card>
+        <div
+            className={`relative caret-grey-800 hover:shadow-[0_0_0_1px] hover:shadow-green ${isFocused ? 'shadow-[0_0_0_1px] shadow-green' : ''}`}
+            ref={ref}
+            data-kg-card
+        >
             {children}
         </div>
     );
