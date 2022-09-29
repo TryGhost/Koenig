2f02d8ff773509ecb7e6c3cf43d8a23cd0b009f4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-29 10:21:53 +0100

    added initial clear-selected-on-blur

--- packages/koenig-lexical/src/components/KoenigCardWrapper.jsx
+++ packages/koenig-lexical/src/components/KoenigCardWrapper.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {mergeRegister} from '@lexical/utils';
 import {useLexicalNodeSelection} from '@lexical/react/useLexicalNodeSelection';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {$getSelection, $isNodeSelection, CLICK_COMMAND, COMMAND_PRIORITY_LOW} from 'lexical';
+import {$getSelection, $isNodeSelection, BLUR_COMMAND, CLICK_COMMAND, COMMAND_PRIORITY_LOW} from 'lexical';
 
 const KoenigCardWrapperComponent = ({nodeKey, children}) => {
     const [editor] = useLexicalComposerContext();
-    const [isSelected, setSelected] = useLexicalNodeSelection(nodeKey);
+    const [isSelected, setSelected, clearSelected] = useLexicalNodeSelection(nodeKey);
     const [selection, setSelection] = React.useState(null);
     const ref = React.useRef(null);
 
@@ -18,13 +18,28 @@ const KoenigCardWrapperComponent = ({nodeKey, children}) => {
             editor.registerCommand(
                 CLICK_COMMAND,
                 (event) => {
-                    setSelected(ref.current.contains(event.target));
+                    if (ref.current.contains(event.target)) {
+                        clearSelected();
+                        setSelected(true);
+                    } else if (isSelected) {
+                        clearSelected();
+                    }
+                    return false;
+                },
+                COMMAND_PRIORITY_LOW
+            ),
+            editor.registerCommand(
+                BLUR_COMMAND,
+                (event) => {
+                    if (isSelected && !ref.current.contains(event.relatedTarget)) {
+                        setSelected(false);
+                    }
                     return false;
                 },
                 COMMAND_PRIORITY_LOW
             )
         );
-    }, [editor, isSelected, setSelected, nodeKey]);
+    }, [editor, isSelected, setSelected, clearSelected, nodeKey]);
 
     const isFocused = $isNodeSelection(selection) && isSelected;
 
