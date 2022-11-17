1844646b8355fd5fd70cd7427dfae13ebddd7e56
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-11-17 13:56:18 +0700

    Added URL paste to link support ref https://github.com/TryGhost/Team/issues/2228
    - Overrides the default lexical PASTE command.
    - Checks if clipboard data is text and if it's a valid URL.
    - Checks if we have text selected.
    - Converts selected text into a new Link Node from the pasted URL.

---
+++ packages/koenig-lexical/test/e2e/text-transforms/links.test.js
@@ -0,0 +1,38 @@
+import {afterAll, beforeAll, beforeEach, describe, test} from 'vitest';
+import {startApp, initialize, focusEditor, assertHTML, html, pasteText} from '../../utils/e2e';
+
+describe('Links', async () => {
+    let app;
+    let page;
+
+    beforeAll(async () => {
+        ({app, page} = await startApp());
+    });
+
+    afterAll(async () => {
+        await app.stop();
+    });
+
+    beforeEach(async () => {
+        await initialize({page});
+    });
+
+    test('converts selected text to link on url paste', async function () {
+        await focusEditor(page);
+        await page.keyboard.type('link');
+        await page.keyboard.down('Shift');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.up('Shift');
+        await pasteText(page, 'https://koenig.ghost.org');
+        await assertHTML(page, html`
+            <p dir="ltr">
+                <a href="https://koenig.ghost.org" dir="ltr">
+                <span data-lexical-text="true">link</span>
+                </a>
+            </p>
+        `);
+    });
+});
