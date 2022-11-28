f0ec70f0c959c18e668f04d38c947e91e52d8005
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-11-28 20:26:52 +0000

    fixed json import assert eslint error

--- packages/kg-default-nodes/.eslintrc.cjs
+++ packages/kg-default-nodes/.eslintrc.cjs
@@ -3,8 +3,15 @@ module.exports = {
     extends: [
         'plugin:ghost/node'
     ],
+    parser: '@babel/eslint-parser',
     parserOptions: {
-        sourceType: 'module'
+        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+            plugins: [
+                '@babel/plugin-syntax-import-assertions'
+            ]
+        }
     },
     env: {
         browser: true,
