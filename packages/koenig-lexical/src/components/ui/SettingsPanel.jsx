26198e877362a7b6bfa354e3089115a2f0d4a568
Rishabh <zrishabhgarg@gmail.com>
2023-01-30 11:04:22 +0530

    fixup! Added useMovable hook to allow drag and reposition

--- packages/koenig-lexical/src/components/ui/SettingsPanel.jsx
+++ packages/koenig-lexical/src/components/ui/SettingsPanel.jsx
@@ -17,11 +17,11 @@ export function SettingsPanel() {
 
 export function ToggleSetting({label, description}) {
     return (
-        <div className="border-b-grey-200 flex w-full items-center justify-between border-b p-6 text-[1.3rem] last-of-type:border-b-0">
+        <div className="flex w-full items-center justify-between border-b border-b-grey-200 p-6 text-[1.3rem] last-of-type:border-b-0">
             <div>
-                <div className="text-grey-900 font-bold">{label}</div>
+                <div className="font-bold text-grey-900">{label}</div>
                 {description &&
-                    <p className="text-grey-700 text-[1.25rem] font-normal leading-snug">{description}</p>
+                    <p className="text-[1.25rem] font-normal leading-snug text-grey-700">{description}</p>
                 }
             </div>
             <div className="shrink-0 pl-2">
@@ -33,11 +33,11 @@ export function ToggleSetting({label, description}) {
 
 export function InputSetting({label, description, value, placeholder}) {
     return (
-        <div className="border-b-grey-200 flex w-full flex-col justify-between gap-2 border-b p-6 text-[1.3rem] last-of-type:border-b-0">
-            <div className="text-grey-900 font-bold">{label}</div>
+        <div className="flex w-full flex-col justify-between gap-2 border-b border-b-grey-200 p-6 text-[1.3rem] last-of-type:border-b-0">
+            <div className="font-bold text-grey-900">{label}</div>
             <Input value={value} placeholder={placeholder} />
             {description &&
-                    <p className="text-grey-700 text-[1.25rem] font-normal leading-snug">{description}</p>
+                    <p className="text-[1.25rem] font-normal leading-snug text-grey-700">{description}</p>
             }
         </div>
     );
