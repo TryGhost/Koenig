21d2962ab83e1c34d9b7fda21b43bdc81ab3482d
Rishabh <zrishabhgarg@gmail.com>
2023-02-27 12:48:08 +0530

    Added caption input keyboard handling
    refs https://github.com/TryGhost/Team/issues/2059
    
    - adds new plugin for caption editor to control its behavior based on certain commands
    - on focus/blur on caption editor, sets the caption focus state to update behavior of the card
    - passes up the enter command so a next paragraph can be added
    - updates image node to handle toolbar items based on caption selection

--- packages/koenig-lexical/src/components/KoenigCaptionEditor.jsx
+++ packages/koenig-lexical/src/components/KoenigCaptionEditor.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useCallback, useContext} from 'react';
 import {HtmlOutputPlugin, KoenigComposableEditor, KoenigComposer, MINIMAL_NODES, MINIMAL_TRANSFORMERS, RestrictContentPlugin} from '../index.js';
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+import {$setSelection, BLUR_COMMAND, COMMAND_PRIORITY_LOW, FOCUS_COMMAND, KEY_ENTER_COMMAND} from 'lexical';
+import CardContext from '../context/CardContext.jsx';
+import {mergeRegister} from '@lexical/utils';
 
 const Placeholder = ({text = 'Type here'}) => {
     return (
@@ -9,7 +13,66 @@ const Placeholder = ({text = 'Type here'}) => {
     );
 };
 
+function CaptionPlugin({parentEditor}) {
+    const [editor] = useLexicalComposerContext();
+    const {setCaptionHasFocus, captionHasFocus} = useContext(CardContext);
+
+    // focus on caption editor when something is typed while card is selected
+    const handleKeyDown = useCallback((event) => {
+        // only if key is printable key, focus on editor
+        if (!captionHasFocus && event.key.length === 1 && !event.ctrlKey && !event.metaKey && !event.altKey) {
+            editor.focus();
+        }
+    }, [editor, captionHasFocus]);
+
+    React.useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [handleKeyDown, editor]);
+
+    // handle focus/blur and enter key commands
+    React.useEffect(
+        () => {
+            return mergeRegister(
+                editor.registerCommand(
+                    FOCUS_COMMAND,
+                    () => {
+                        setCaptionHasFocus(true);
+                        return false;
+                    },
+                    COMMAND_PRIORITY_LOW
+                ),
+                editor.registerCommand(
+                    BLUR_COMMAND,
+                    () => {
+                        setCaptionHasFocus(false);
+                        editor.update(() => {
+                            $setSelection(null);
+                        });
+                        return false;
+                    },
+                    COMMAND_PRIORITY_LOW
+                ),
+                editor.registerCommand(
+                    KEY_ENTER_COMMAND,
+                    (event) => {
+                        parentEditor?.dispatchCommand(KEY_ENTER_COMMAND, event);
+                        return false;
+                    },
+                    COMMAND_PRIORITY_LOW
+                )
+            );
+        },
+        [editor, setCaptionHasFocus, mainEditor]
+    );
+
+    return null;
+}
+
 const KoenigCaptionEditor = ({paragraphs = 1, html, setHtml, placeholderText, readOnly}) => {
+    const [parentEditor] = useLexicalComposerContext();
     return (
         <KoenigComposer
             nodes={MINIMAL_NODES}
@@ -20,6 +83,7 @@ const KoenigCaptionEditor = ({paragraphs = 1, html, setHtml, placeholderText, re
                 placeholder={<Placeholder text={placeholderText} />}
                 readOnly={readOnly}
             >
+                <CaptionPlugin parentEditor={parentEditor} />
                 <RestrictContentPlugin paragraphs={paragraphs} />
                 <HtmlOutputPlugin html={html} setHtml={setHtml} />
             </KoenigComposableEditor>
