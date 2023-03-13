8cd55d17719b54bb86ce8f1b8cf2936312c38fa3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2023-03-13 17:28:47 +0000

    fixed class order

--- packages/koenig-lexical/src/components/KoenigCaptionEditor.jsx
+++ packages/koenig-lexical/src/components/KoenigCaptionEditor.jsx
@@ -7,7 +7,7 @@ import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 
 const Placeholder = ({text = 'Type here'}) => {
     return (
-        <div className="text-grey-500 pointer-events-none absolute top-0 left-0 m-0 min-w-full cursor-text font-sans text-sm font-normal tracking-wide ">
+        <div className="pointer-events-none absolute top-0 left-0 m-0 min-w-full cursor-text font-sans text-sm font-normal tracking-wide text-grey-500 ">
             {text}
         </div>
     );
