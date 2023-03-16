620e30678abd341c6f8a871e6f2a7116891cfe93
Sagar Gupta <guptazy@gmail.com>
2023-03-16 14:52:46 +0100

    Fix css

--- packages/koenig-lexical/src/components/ui/cards/ToggleCard.jsx
+++ packages/koenig-lexical/src/components/ui/cards/ToggleCard.jsx
@@ -14,20 +14,18 @@ export function ToggleCard({
 }) {
     return (
         <div className="border-grey/40 rounded border py-4 px-6">
-            <div className="flex items-start justify-between">
-                { (isEditing || header) &&
-                    <div className={`mr-2 font-sans text-xl font-bold leading-snug text-black ${header ? 'opacity-100' : 'opacity-40'}`}>
-                        {header || headerPlaceholder}
-                    </div>
-                }
-                <div className="ml-auto flex h-8 w-8 shrink-0 items-center justify-center">
+            <div className="w-100 flex justify-between">
+                <div className={`font-sans text-xl font-bold leading-relaxed text-black ${header ? 'opacity-100' : 'opacity-40'}`}>
+                    <span onChange={setHeader}>{header || headerPlaceholder}</span>
+                </div>
+                <div className="my-2 ml-auto">
                     <ArrowDownIcon className="text-grey-400 h-4 w-4 stroke-2" />
                 </div>
             </div>
             { (isEditing || content) &&
-                <div className={`text-grey-900 mt-2 w-full font-serif text-xl font-normal leading-relaxed ${content ? 'opacity-100' : 'opacity-40'} `}>
-                    {content || contentPlaceholder}
-                </div>
+            <div className={`text-grey-900 mt-2 w-full font-serif text-xl font-normal leading-relaxed ${content ? 'opacity-100' : 'opacity-40'} `}>
+                {content || contentPlaceholder}
+            </div>
             }
         </div>
     );
@@ -41,6 +39,12 @@ ToggleCard.propTypes = {
     setContent: PropTypes.func,
     setHeader: PropTypes.func,
     isEditing: PropTypes.bool,
-    isSelected: PropTypes.bool,
-    isCollapsed: PropTypes.bool
+    isSelected: PropTypes.bool
+};
+
+ToggleCard.defaultProps = {
+    content: '',
+    contentPlaceholder: 'Collapsible content',
+    header: '',
+    headerPlaceholder: 'Toggle header'
 };
