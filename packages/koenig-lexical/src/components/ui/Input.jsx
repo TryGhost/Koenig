086695dfa259640e0e5d5ffa61e9c401bda0c347
Steve Larson <9larsons@gmail.com>
2023-03-22 10:46:15 -0500

    added support for state interaction

--- packages/koenig-lexical/src/components/ui/Input.jsx
+++ packages/koenig-lexical/src/components/ui/Input.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export function Input({value, placeholder}) {
+export function Input({value, placeholder, onChange}) {
     return (
-        <input className="w-full rounded border border-grey-300 p-2 font-sans text-sm font-normal leading-snug text-grey-900 focus-visible:outline-none" placeholder={placeholder} value={value} />
+        <input
+            className="w-full rounded border border-grey-300 p-2 font-sans text-sm font-normal leading-snug text-grey-900 focus-visible:outline-none"
+            defaultValue={value}
+            placeholder={placeholder}
+            onChange={onChange}
+        />
     );
 }
