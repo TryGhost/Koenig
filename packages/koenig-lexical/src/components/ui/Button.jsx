64f084f9c6b24a3ab5154f056827a492875c40f8
Steve Larson <9larsons@gmail.com>
2023-03-24 12:14:56 -0500

    changed styling use

--- packages/koenig-lexical/src/components/ui/Button.jsx
+++ packages/koenig-lexical/src/components/ui/Button.jsx
@@ -2,17 +2,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export function Button({color, dataTestId, href, size, width, value, placeholder, type = 'button'}) {
-    const linkStyle = {
-        color: 'inherit',
-        textDecoration: 'none'
-    };
-
     return (
         // eslint-disable-next-line react/button-has-type
-        <button className={`inline-block rounded font-sans font-medium ${(width === 'regular') || 'w-full' } ${value ? 'opacity-100' : 'opacity-50' } ${(color === 'light') ? 'bg-white text-black' : 'bg-green text-white'} `} data-testid={`${dataTestId}`} placeholder={placeholder} type={type} value={value}>
+        <button className={`not-kg-prose inline-block rounded font-sans font-medium ${(width === 'regular') || 'w-full' } ${value ? 'opacity-100' : 'opacity-50' } ${(color === 'light') ? 'bg-white text-black' : 'bg-green text-white'} `} data-testid={`${dataTestId}`} placeholder={placeholder} type={type} value={value}>
             {href ? (
                 <span className={`block px-5 ${(size === 'small' ? 'h-10 text-md leading-[4rem]' : (size === 'medium' ? 'h-11 text-[1.6rem] leading-[4.4rem]' : 'h-12 text-lg leading-[4.8rem]'))}`} data-testid={`${dataTestId}-span`}>
-                    <a data-testid={`${dataTestId}-link`} href={href} style={linkStyle}>{value || placeholder}</a>
+                    <a data-testid={`${dataTestId}-link`} href={href}>{value || placeholder}</a>
                 </span>
             ) : (
                 <span className={`block px-5 ${(size === 'small' ? 'h-10 text-md leading-[4rem]' : (size === 'medium' ? 'h-11 text-[1.6rem] leading-[4.4rem]' : 'h-12 text-lg leading-[4.8rem]'))}`} data-testid={`${dataTestId}-span`}>
