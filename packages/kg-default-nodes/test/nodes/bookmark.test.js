44b718e7ec6810f008e308c0c530d54bc2568c28
Steve Larson <9larsons@gmail.com>
2023-03-29 08:06:55 -0500

    exportDOM test still not working

--- packages/kg-default-nodes/test/nodes/bookmark.test.js
+++ packages/kg-default-nodes/test/nodes/bookmark.test.js
@@ -3,12 +3,13 @@ const {$getRoot} = require('lexical');
 const {createHeadlessEditor} = require('@lexical/headless');
 const {$generateNodesFromDOM} = require('@lexical/html');
 const {JSDOM} = require('jsdom');
+const Prettier = require('prettier');
 
 const {BookmarkNode, $createBookmarkNode, $isBookmarkNode} = require('../../');
 
 const editorNodes = [BookmarkNode];
 
-describe.only('BookmarkNode', function () {
+describe('BookmarkNode', function () {
     let editor;
     let dataset;
     let exportOptions;
@@ -121,22 +122,44 @@ describe.only('BookmarkNode', function () {
         }));
     });
 
-    // describe('exportDOM', function () {
-    //     it('creates an bookmark card', editorTest(function () {
-    //         const bookmarkNode = $createBookmarkNode(dataset);
-    //         const {element} = bookmarkNode.exportDOM(exportOptions);
+    describe('exportDOM', function () {
+        it.only('creates an bookmark card', editorTest(function () {
+            const bookmarkNode = $createBookmarkNode(dataset);
+            const {element} = bookmarkNode.exportDOM(exportOptions);
 
-    //         element.outerHTML.should.prettifyTo(html`<div class="kg-card kg-bookmark-card"><img src="/content/images/2022/11/koenig-bookmark-lexical.jpg" alt="bookmark-thumbnail" class="kg-bookmark-thumbnail"><div class="kg-bookmark-thumbnail placeholder kg-bookmark-hide"><svg width="24" height="24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 15.33a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM15 13.83a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0Z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M14.486 6.81A2.25 2.25 0 0 1 17.25 9v5.579a.75.75 0 0 1-1.5 0v-5.58a.75.75 0 0 0-.932-.727.755.755 0 0 1-.059.013l-4.465.744a.75.75 0 0 0-.544.72v6.33a.75.75 0 0 1-1.5 0v-6.33a2.25 2.25 0 0 1 1.763-2.194l4.473-.746Z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M3 1.5a.75.75 0 0 0-.75.75v19.5a.75.75 0 0 0 .75.75h18a.75.75 0 0 0 .75-.75V5.133a.75.75 0 0 0-.225-.535l-.002-.002-3-2.883A.75.75 0 0 0 18 1.5H3ZM1.409.659A2.25 2.25 0 0 1 3 0h15a2.25 2.25 0 0 1 1.568.637l.003.002 3 2.883a2.25 2.25 0 0 1 .679 1.61V21.75A2.25 2.25 0 0 1 21 24H3a2.25 2.25 0 0 1-2.25-2.25V2.25c0-.597.237-1.169.659-1.591Z"></path></svg></div><div class="kg-bookmark-player-container"><bookmark src="/content/bookmark/2022/11/koenig-lexical.mp3" preload="metadata"></bookmark><div class="kg-bookmark-title">Test Bookmark</div><div class="kg-bookmark-player"><button class="kg-bookmark-play-icon"><svg viewBox="0 0 24 24"><path d="M23.14 10.608 2.253.164A1.559 1.559 0 0 0 0 1.557v20.887a1.558 1.558 0 0 0 2.253 1.392L23.14 13.393a1.557 1.557 0 0 0 0-2.785Z"></path></svg></button><button class="kg-bookmark-pause-icon kg-bookmark-hide"><svg viewBox="0 0 24 24"><rect x="3" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect><rect x="14" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect></svg></button><span class="kg-bookmark-current-time">0:00</span><div class="kg-bookmark-time">/<span class="kg-bookmark-duration">60</span></div><input type="range" class="kg-bookmark-seek-slider" max="100" value="0"><button class="kg-bookmark-playback-rate">1×</button><button class="kg-bookmark-unmute-icon"><svg viewBox="0 0 24 24"><path d="M15.189 2.021a9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1.794a.249.249 0 0 1 .221.133 9.73 9.73 0 0 0 7.924 4.85h.06a1 1 0 0 0 1-1V3.02a1 1 0 0 0-1.06-.998Z"></path></svg></button><button class="kg-bookmark-mute-icon kg-bookmark-hide"><svg viewBox="0 0 24 24"><path d="M16.177 4.3a.248.248 0 0 0 .073-.176v-1.1a1 1 0 0 0-1.061-1 9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h.114a.251.251 0 0 0 .177-.073ZM23.707 1.706A1 1 0 0 0 22.293.292l-22 22a1 1 0 0 0 0 1.414l.009.009a1 1 0 0 0 1.405-.009l6.63-6.631A.251.251 0 0 1 8.515 17a.245.245 0 0 1 .177.075 10.081 10.081 0 0 0 6.5 2.92 1 1 0 0 0 1.061-1V9.266a.247.247 0 0 1 .073-.176Z"></path></svg></button><input type="range" class="kg-bookmark-volume-slider" max="100" value="100"></div></div></div>`);
-    //     }));
+            const expectedHtml = `
+                <figure class="kg-card kg-bookmark-card kg-card-hascaption">
+                    <a class="kg-bookmark-container" href="${dataset.url}">
+                        <div class="kg-bookmark-content">
+                            <div class="kg-bookmark-title">${dataset.title}</div>
+                            <div class="kg-bookmark-description">${dataset.description}</div>
+                            <div class="kg-bookmark-metadata">
+                                <img class="kg-bookmark-icon" src="${dataset.icon}" alt="">
+                                <span class="kg-bookmark-author">${dataset.author}</span>
+                                <span class="kg-bookmark-publisher">${dataset.publisher}</span>
+                            </div>
+                        </div>
+                        <div class="kg-bookmark-thumbnail">
+                            <img src="${dataset.thumbnail}" alt="">
+                        </div>
+                    </a>
+                    <figcaption>${dataset.caption}</figcaption>
+                </figure>
+            `;
 
-    //     it('renders nothing with a missing src', editorTest(function () {
-    //         const bookmarkNode = $createBookmarkNode();
-    //         const {element} = bookmarkNode.exportDOM(exportOptions);
+            const prettyExpectedHtml = Prettier.format(expectedHtml, {parser: 'html'});
 
-    //         element.textContent.should.equal('');
-    //         should(element.outerHTML).be.undefined();
-    //     }));
-    // });
+            element.outerHTML.should.prettifyTo(prettyExpectedHtml);
+        })); 
+
+        it('renders nothing with a missing src', editorTest(function () {
+            const bookmarkNode = $createBookmarkNode();
+            const {element} = bookmarkNode.exportDOM(exportOptions);
+
+            element.textContent.should.equal('');
+            should(element.outerHTML).be.undefined();
+        }));
+    });
 
     describe('exportJSON', function () {
         it('contains all data', editorTest(function () {
@@ -228,18 +251,38 @@ describe.only('BookmarkNode', function () {
         }));
     });
 
-    // describe('importDOM', function () {
-    //     it('parses bookmark card', editorTest(function () {
-    //         const dom = (new JSDOM(html`
-    //                     <div class="kg-card kg-bookmark-card"><img src="/content/images/2022/11/koenig-bookmark-lexical.jpg" alt="bookmark-thumbnail" class="kg-bookmark-thumbnail"><div class="kg-bookmark-thumbnail placeholder kg-bookmark-hide"><svg width="24" height="24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 15.33a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM15 13.83a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0Z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M14.486 6.81A2.25 2.25 0 0 1 17.25 9v5.579a.75.75 0 0 1-1.5 0v-5.58a.75.75 0 0 0-.932-.727.755.755 0 0 1-.059.013l-4.465.744a.75.75 0 0 0-.544.72v6.33a.75.75 0 0 1-1.5 0v-6.33a2.25 2.25 0 0 1 1.763-2.194l4.473-.746Z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M3 1.5a.75.75 0 0 0-.75.75v19.5a.75.75 0 0 0 .75.75h18a.75.75 0 0 0 .75-.75V5.133a.75.75 0 0 0-.225-.535l-.002-.002-3-2.883A.75.75 0 0 0 18 1.5H3ZM1.409.659A2.25 2.25 0 0 1 3 0h15a2.25 2.25 0 0 1 1.568.637l.003.002 3 2.883a2.25 2.25 0 0 1 .679 1.61V21.75A2.25 2.25 0 0 1 21 24H3a2.25 2.25 0 0 1-2.25-2.25V2.25c0-.597.237-1.169.659-1.591Z"></path></svg></div><div class="kg-bookmark-player-container"><bookmark src="/content/bookmark/2022/11/koenig-lexical.mp3" preload="metadata"></bookmark><div class="kg-bookmark-title">Test Bookmark</div><div class="kg-bookmark-player"><button class="kg-bookmark-play-icon"><svg viewBox="0 0 24 24"><path d="M23.14 10.608 2.253.164A1.559 1.559 0 0 0 0 1.557v20.887a1.558 1.558 0 0 0 2.253 1.392L23.14 13.393a1.557 1.557 0 0 0 0-2.785Z"></path></svg></button><button class="kg-bookmark-pause-icon kg-bookmark-hide"><svg viewBox="0 0 24 24"><rect x="3" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect><rect x="14" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect></svg></button><span class="kg-bookmark-current-time">0:00</span><div class="kg-bookmark-time">/<span class="kg-bookmark-duration">60</span></div><input type="range" class="kg-bookmark-seek-slider" max="100" value="0"><button class="kg-bookmark-playback-rate">1&amp;#215;</button><button class="kg-bookmark-unmute-icon"><svg viewBox="0 0 24 24"><path d="M15.189 2.021a9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1.794a.249.249 0 0 1 .221.133 9.73 9.73 0 0 0 7.924 4.85h.06a1 1 0 0 0 1-1V3.02a1 1 0 0 0-1.06-.998Z"></path></svg></button><button class="kg-bookmark-mute-icon kg-bookmark-hide"><svg viewBox="0 0 24 24"><path d="M16.177 4.3a.248.248 0 0 0 .073-.176v-1.1a1 1 0 0 0-1.061-1 9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h.114a.251.251 0 0 0 .177-.073ZM23.707 1.706A1 1 0 0 0 22.293.292l-22 22a1 1 0 0 0 0 1.414l.009.009a1 1 0 0 0 1.405-.009l6.63-6.631A.251.251 0 0 1 8.515 17a.245.245 0 0 1 .177.075 10.081 10.081 0 0 0 6.5 2.92 1 1 0 0 0 1.061-1V9.266a.247.247 0 0 1 .073-.176Z"></path></svg></button><input type="range" class="kg-bookmark-volume-slider" max="100" value="100"></div></div></div>
-    //         `)).window.document;
-    //         const nodes = $generateNodesFromDOM(editor, dom);
-    //         nodes.length.should.equal(1);
-    //         nodes[0].getSrc().should.equal('/content/bookmark/2022/11/koenig-lexical.mp3');
-    //         nodes[0].getThumbnailSrc().should.equal('/content/images/2022/11/koenig-bookmark-lexical.jpg');
-    //         nodes[0].getDuration().should.equal(3600);
-    //         nodes[0].getTitle().should.equal('Test Bookmark');
-    //         nodes[0].getMimeType().should.equal('');
-    //     }));
-    // });
+    describe('importDOM', function () {
+        it('parses bookmark card', editorTest(function () {
+            const dom = (new JSDOM(html`
+                <figure class="kg-card kg-bookmark-card kg-card-hascaption">
+                    <a class="kg-bookmark-container" href="${dataset.url}">
+                        <div class="kg-bookmark-content">
+                            <div class="kg-bookmark-title">${dataset.title}</div>
+                            <div class="kg-bookmark-description">${dataset.description}</div>
+                            <div class="kg-bookmark-metadata">
+                                <img class="kg-bookmark-icon" src="${dataset.icon}" alt="">
+                                <span class="kg-bookmark-author">${dataset.author}</span>
+                                <span class="kg-bookmark-publisher">${dataset.publisher}</span>
+                            </div>
+                        </div>
+                        <div class="kg-bookmark-thumbnail">
+                            <img src="${dataset.thumbnail}" alt="">
+                        </div>
+                    </a>
+                    <figcaption>${dataset.caption}</figcaption>
+                </figure>
+            `)).window.document;
+            const nodes = $generateNodesFromDOM(editor, dom);
+
+            nodes.length.should.equal(1);
+            nodes[0].getUrl().should.equal(dataset.url);
+            nodes[0].getIcon().should.equal(dataset.icon);
+            nodes[0].getTitle().should.equal(dataset.title);
+            nodes[0].getDescription().should.equal(dataset.description);
+            nodes[0].getAuthor().should.equal(dataset.author);
+            nodes[0].getPublisher().should.equal(dataset.publisher);
+            nodes[0].getThumbnail().should.equal(dataset.thumbnail);
+            nodes[0].getCaption().should.equal(dataset.caption);
+        }));
+    });
 });
