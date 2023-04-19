163b57e5c98faea490dde7f4e78fd86b492b73a0
Steve Larson <9larsons@gmail.com>
2023-04-19 16:42:00 +0100

    basic resizeobserver

--- packages/koenig-lexical/src/components/ui/cards/EmbedCard.jsx
+++ packages/koenig-lexical/src/components/ui/cards/EmbedCard.jsx
@@ -37,7 +37,54 @@ export function EmbedCard({captionEditor, captionEditorInitialState, html, isSel
 }
 
 function EmbedIframe({dataTestId, html}) {
-    return <iframe className="bn miw-100" data-testid={dataTestId} srcDoc={html} title="embed-card-iframe"></iframe>;
+    // const [width, setWidth] = React.useState();
+    // const [height, setHeight] = React.useState();
+    const iframeRef = React.useRef(null);
+
+    const handleResize = () => {
+        iframeRef.current.style.height = null;
+
+        // NOTE: this does not load the embedded iframe content quickly enough... we may need to make
+        // this a promise and wait for the iframe to load before we can get the ratio
+
+        // get ratio from nested iframe if present (eg, Vimeo)
+        const firstElement = iframeRef.current.contentDocument.body.firstChild;
+        console.log(`firstElement`, firstElement);
+        if (firstElement.tagName === 'IFRAME') {
+            const widthAttr = firstElement.getAttribute('width');
+
+            if (widthAttr.indexOf('%') === -1) {
+                const width = parseInt(firstElement.getAttribute('width'));
+                const height = parseInt(firstElement.getAttribute('height'));
+                if (width && height) {
+                    const ratio = width / height;
+                    const newHeight = iframeRef.current.offsetWidth / ratio;
+                    firstElement.style.height = `${newHeight}px`;
+                    iframeRef.current.style.height = `${newHeight}px`;
+                    return;
+                }
+            }
+
+            const heightAttr = firstElement.getAttribute('height');
+            if (heightAttr.indexOf('%') === -1) {
+                const height = parseInt(firstElement.getAttribute('height'));
+                iframeRef.current.style.height = `${height}px`;
+                return;
+            }
+        }
+    };
+
+    React.useEffect(() => {
+        const resizeObserver = new ResizeObserver(handleResize);
+        console.log(`observing`, iframeRef.current);
+        resizeObserver.observe(iframeRef.current);
+
+        return function cleanup() {
+            resizeObserver.disconnect();
+        };
+    }, []);
+
+    return <iframe ref={iframeRef} className="bn miw-100" data-testid={dataTestId} srcDoc={html} title="embed-card-iframe"></iframe>;
 }
 
 EmbedCard.propTypes = {
