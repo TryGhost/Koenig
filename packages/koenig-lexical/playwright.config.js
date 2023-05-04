dddf21753a550068972f9a40df860886dece034e
Elena Baidakova <elena@ghost.org>
2023-05-04 16:04:36 +0400

    Increase amount of workers on CI
    refs TryGhost/Team#3124

--- packages/koenig-lexical/playwright.config.js
+++ packages/koenig-lexical/playwright.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 2 : undefined,
+    workers: process.env.CI ? 4 : undefined,
     maxFailures: process.env.CI ? 10 : 0,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['html'], [process.env.CI ? 'github' : 'list']],
