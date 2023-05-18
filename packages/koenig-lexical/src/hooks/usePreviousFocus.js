495aa25105f34bbb5f952aeb37ec31f61ac08da3
Sag <guptazy@gmail.com>
2023-05-18 08:29:02 +0200

    Used ref instead of state

--- packages/koenig-lexical/src/hooks/usePreviousFocus.js
+++ packages/koenig-lexical/src/hooks/usePreviousFocus.js
@@ -1,22 +1,22 @@
-import {useState} from 'react';
+import {useRef} from 'react';
 
 export const usePreviousFocus = (onClick, name) => {
-    const [previousRange, setPreviousRange] = useState(null);
+    const previousRangeRef = useRef(null);
 
     const handleMousedown = () => {
         const selection = document.getSelection();
-        setPreviousRange(selection.rangeCount === 0 ? null : selection.getRangeAt(0));
+        previousRangeRef.current = (selection.rangeCount === 0 ? null : selection.getRangeAt(0));
     };
 
     const handleClick = (e) => {
         e.preventDefault();
         onClick(name);
 
-        if (previousRange) {
+        if (previousRangeRef.current) {
             const selection = document.getSelection();
             selection.removeAllRanges();
-            selection.addRange(previousRange);
-            setPreviousRange(null);
+            selection.addRange(previousRangeRef.current);
+            previousRangeRef.current = null;
         }
     };
 
