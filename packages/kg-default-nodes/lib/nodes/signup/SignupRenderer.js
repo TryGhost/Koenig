ee9b50596a79859b4fbd009e83d58f044c552c3f
Sanne de Vries <sannedv@protonmail.com>
2023-05-19 11:13:01 +0200

    Updated frontend rendering of signup card
    Refs https://github.com/TryGhost/Team/issues/3246

--- packages/kg-default-nodes/lib/nodes/signup/SignupRenderer.js
+++ packages/kg-default-nodes/lib/nodes/signup/SignupRenderer.js
@@ -4,16 +4,20 @@ import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 // which elements should be customizable inside the editor
 // ref https://ghost.org/docs/themes/members#signup-forms
 
-function cardTemplate(nodeData) {
+function cardTemplate({nodeData, node}) {
+    const cardClasses = getCardClasses(node).join(' ');
+
     return `
-    <div data-lexical-signup-form style="display:none">
-        <h1>${nodeData.header}</h1>
-        <h2>${nodeData.subheader}</h2>
-        <p>${nodeData.disclaimer}</p>
-        <form data-members-form="" style="background-image: url(${nodeData.backgroundImageSrc});">
-            <input id="email" data-members-email="" type="email" required="true" />
-            <button type="submit">${nodeData.buttonText}</button>
-        </form>
+    <div class="${cardClasses}" data-lexical-signup-form style="display:none;">
+        <div class="kg-signup-card-container" style="background-color: ${nodeData.backgroundColor};">
+            <h1 style="color: ${nodeData.textColor};">${nodeData.header}</h1>
+            <h2 style="color: ${nodeData.textColor};">${nodeData.subheader}</h2>
+            <form data-members-form="" style="background-image: url(${nodeData.backgroundImageSrc});">
+                <input style="border-color: ${nodeData.buttonColor};" id="email" data-members-email="" type="email" required="true" />
+                <button type="submit" style="background-color: ${nodeData.buttonColor}; color: ${nodeData.buttonTextColor};">${nodeData.buttonText || 'Subscribe'}</button>
+            </form>
+            <p style="color: ${nodeData.textColor};">${nodeData.disclaimer}</p>
+        </div>
     </div>
     `;
 }
@@ -44,4 +48,14 @@ export function renderSignupCardToDOM(dataset, options = {}) {
     const element = document.createElement('div');
     element.innerHTML = htmlString?.trim();
     return element.firstElementChild;
+}
+
+export function getCardClasses(node) {
+    let cardClasses = ['kg-card kg-signup-card'];
+
+    if (node.getCardWidth()) {
+        cardClasses.push(`kg-width-${node.getCardWidth()}`);
+    }
+
+    return cardClasses;
 }
