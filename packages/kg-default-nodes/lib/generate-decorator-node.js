14de266adc795f4a672c31cec1de8211ca96a8f3
Sag <guptazy@gmail.com>
2023-06-08 10:12:02 +0200

    Added reference material in generateDecoratorNode

--- packages/kg-default-nodes/lib/generate-decorator-node.js
+++ packages/kg-default-nodes/lib/generate-decorator-node.js
@@ -4,16 +4,18 @@ import {KoenigDecoratorNode} from './KoenigDecoratorNode';
  * @typedef {Object} DecoratorNodeProperty
  * @property {string} name - The property's name.
  * @property {string} type - The property's type.
- * @property {*} default - The property's default value.
- * @property {string|null} urlType - If the property contains a URL, the URL's type (e.g. 'url', 'html', 'markdown')
+ * @property {*} default - The property's default value
+ * @property {('url'|'html'|'markdown'|null)} urlType - If the property contains a URL, the URL's type: 'url', 'html' or 'markdown'. Use 'url' is the property contains only a URL, 'html' or 'markdown' if the property contains HTML or markdown code, that may contain URLs.
  *
  * @param {string} nodeType – The node's type (must be unique)
  * @param {DecoratorNodeProperty[]} properties - An array of properties for the generated class
  * @returns {Object} - The generated class.
  */
-export function generateDecoratorNode({nodeType = '', properties = []}) {
-    properties = properties.map((obj) => {
-        return {...obj, privateName: `__${obj.name}`};
+export function generateDecoratorNode({nodeType, properties = []}) {
+    // Adds a `privateName` field to the properties for convenience (e.g. `__name`):
+    // properties: [{name: 'name', privateName: '__name', type: 'string', default: 'hello'}, {...}]
+    properties = properties.map((prop) => {
+        return {...prop, privateName: `__${prop.name}`};
     });
 
     class GeneratedDecoratorNode extends KoenigDecoratorNode {
@@ -24,17 +26,30 @@ export function generateDecoratorNode({nodeType = '', properties = []}) {
             });
         }
 
+        /**
+         * Returns the node's unique type
+         * @extends DecoratorNode
+         * @see https://lexical.dev/docs/concepts/nodes#extending-decoratornode
+         * @returns {string}
+         */
         static getType() {
             return nodeType;
         }
   
+        /**
+         * Creates a copy of an existing node with all its properties
+         * @extends DecoratorNode
+         * @see https://lexical.dev/docs/concepts/nodes#extending-decoratornode
+         */
         static clone(node) {
             return new this(node.getDataset(), node.__key);
         }
 
-        // Transforms URLs contained in the payload to relative paths (`__GHOST_URL__/relative/path/`),
-        // so that URLs to be changed without having to update the database
-        // (cf. `@tryghost/url-utils` for more information)
+        /**
+         * Transforms URLs contained in the payload to relative paths (`__GHOST_URL__/relative/path/`),
+         * so that URLs to be changed without having to update the database
+         * @see https://github.com/TryGhost/SDK/tree/main/packages/url-utils
+         */
         static get urlTransformMap() {
             let map = {};
 
@@ -47,6 +62,10 @@ export function generateDecoratorNode({nodeType = '', properties = []}) {
             return map;
         }
 
+        /**
+         * Convenience method to get all properties of the node
+         * @returns {Object} - The node's properties
+         */
         getDataset() {
             const self = this.getLatest();
 
@@ -58,6 +77,12 @@ export function generateDecoratorNode({nodeType = '', properties = []}) {
             return dataset;
         }
 
+        /**
+         * Converts JSON to a Lexical node
+         * @see https://lexical.dev/docs/concepts/serialization#lexicalnodeimportjson
+         * @extends DecoratorNode
+         * @param {Object} serializedNode - Lexical's representation of the node, in JSON format
+         */
         static importJSON(serializedNode) {
             const data = {};
 
@@ -68,6 +93,11 @@ export function generateDecoratorNode({nodeType = '', properties = []}) {
             return new this(data);
         }
 
+        /**
+         * Serializes a Lexical node to JSON. The JSON content is then saved to the database.
+         * @extends DecoratorNode
+         * @see https://lexical.dev/docs/concepts/serialization#lexicalnodeexportjson
+         */
         exportJSON() {
             const dataset = {
                 type: nodeType,
@@ -81,28 +111,61 @@ export function generateDecoratorNode({nodeType = '', properties = []}) {
         }
 
         /* c8 ignore start */
+        /**
+         * Inserts node in the DOM. Required when extending the DecoratorNode.
+         * @extends DecoratorNode
+         * @see https://lexical.dev/docs/concepts/nodes#extending-decoratornode
+         */
         createDOM() {
             return document.createElement('div');
         }
 
+        /**
+         * Required when extending the DecoratorNode
+         * @extends DecoratorNode
+         * @see https://lexical.dev/docs/concepts/nodes#extending-decoratornode
+         */
         updateDOM() {
             return false;
         }
 
-        // All our cards are top-level blocks
+        /**
+         * Defines whether a node is a top-level block.
+         * @see https://lexical.dev/docs/api/classes/lexical.DecoratorNode#isinline
+         */
         isInline() {
+            // All our cards are top-level blocks. Override if needed.
             return false;
         }
 
-        // Most of our cards are editable
-        // If not, override this method
+        /**
+         * Defines whether a node has an edit mode in the editor UI
+         */
         hasEditMode() {
+            // Most of our cards have an edit mode. Override if needed.
             return true;
         }
         /* c8 ignore stop */
     }
 
-    // Add getters and setters to the class prototype
+    /**
+     * Generates getters and setters for each property, following ES6 syntax
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
+     *
+     * Example: for a given property 'content', the generated getter and setter will be:
+     * get content() {
+     *    const self = this.getLatest();
+     *    return self.__content;
+     * }
+     *
+     * set content(newVal) {
+     *   const writable = this.getWritable();
+     *   writable.__content = newVal;
+     * }
+     *
+     * They can be used as `node.content` (getter) and `node.content = 'new value'` (setter)
+     */
     properties.forEach((prop) => {
         Object.defineProperty(GeneratedDecoratorNode.prototype, prop.name, {
             get: function () {
