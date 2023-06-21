b94a3dd1a8dd70f5260802bd8b8546348116649c
Sag <guptazy@gmail.com>
2023-06-21 15:10:04 +0200

    Refactored Paywall node

--- packages/kg-default-nodes/lib/nodes/paywall/PaywallRenderer.js
+++ packages/kg-default-nodes/lib/nodes/paywall/PaywallRenderer.js
@@ -1,12 +1,13 @@
 import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 
-export function renderPaywallNodeToDOM(_, options = {}) {
+export function renderPaywallNode(_, options = {}) {
     addCreateDocumentOption(options);
-
     const document = options.createDocument();
-    const div = document.createElement('div');
+    const element = document.createElement('div');
 
-    div.innerHTML = '<!--members-only-->';
+    element.innerHTML = '<!--members-only-->';
 
-    return {element: div};
+    // type: 'inner' will render only the innerHTML of the element
+    // @see https://github.com/TryGhost/Koenig/blob/e14c008e176f7a1036fe3f3deb924ed69a69191f/packages/kg-lexical-html-renderer/lib/convert-to-html-string.js#L29
+    return {element, type: 'inner'};
 }
--- packages/kg-default-nodes/lib/utils/render-empty-container.js
+++ packages/kg-default-nodes/lib/utils/render-empty-container.js
@@ -2,7 +2,7 @@
  * Renders an empty container element
  * In the returned object, `type: 'inner'` is picked up by the `@tryghost/kg-lexical-html-renderer` package
  * to render the inner content of the container element (in this case, nothing)
- * 
+ *
  * @see https://github.com/TryGhost/Koenig/blob/e14c008e176f7a1036fe3f3deb924ed69a69191f/packages/kg-lexical-html-renderer/lib/convert-to-html-string.js#L29
  */
 export function renderEmptyContainer(document) {
