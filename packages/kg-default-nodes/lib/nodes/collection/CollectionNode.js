a8a6150fd36c1922f9d26318bee3ef938717d124
Steve Larson <9larsons@gmail.com>
2023-07-05 13:10:54 -0500

    Added collection node
    refs TryGhost/Team#3565
    -add basic framework for collection node
    -no renderer/parser yet

---
+++ packages/kg-default-nodes/lib/nodes/collection/CollectionNode.js
@@ -0,0 +1,32 @@
+import {generateDecoratorNode} from '../../generate-decorator-node';
+// import {parseCollectionNode} from './CollectionParser';
+// import {renderCollectionNode} from './CollectionRenderer';
+
+export class CollectionNode extends generateDecoratorNode({nodeType: 'collection',
+    properties: [
+        {name: 'collection', default: 'featured'},
+        {name: 'postCount', default: '3'},
+        {name: 'layout', default: 'list'},
+        {name: 'columns', default: '1'},
+        {name: 'rows', default: '3'}
+    ]}
+    // do we want a limit option so we don't have to 'get' and calculate it?
+    // {name: 'limit', default: '3'}
+    // may want other props like showing title, excerpt, date posted, etc.
+) {
+    // static importDOM() {
+    //     return parseCollectionNode(this);
+    // }
+
+    // exportDOM(options = {}) {
+    //     return renderCollectionNode(this, options);
+    // }
+}
+
+export const $createCollectionNode = (dataset) => {
+    return new CollectionNode(dataset);
+};
+
+export function $isCollectionNode(node) {
+    return node instanceof CollectionNode;
+}
