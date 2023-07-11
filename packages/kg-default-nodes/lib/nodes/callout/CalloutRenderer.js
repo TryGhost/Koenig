e9ff6f175918e9b2a7b5a4274bc30940b60a5b82
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-07-11 11:10:46 +0200

    🐛 Added sanitation to callout renderer.
    refs https://github.com/TryGhost/Ghost/issues/17215
    
    - Fixes an issue that caused some templates to render the card incorrectly.
    - Refactored the renderCalloutNode function to parse the calloutText into a DOM tree using jsdom
    - Created a cleanDOM helper function to traverse the DOM tree and remove unwanted HTML tags
    - Retained only 'A', 'STRONG', and 'EM' tags in the HTML string to bring
      it in line with how mobiledoc rendered HTML.

--- packages/kg-default-nodes/lib/nodes/callout/CalloutRenderer.js
+++ packages/kg-default-nodes/lib/nodes/callout/CalloutRenderer.js
@@ -1,3 +1,4 @@
+import {JSDOM} from 'jsdom';
 import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 
 export function renderCalloutNode(node, options = {}) {
@@ -16,8 +17,30 @@ export function renderCalloutNode(node, options = {}) {
 
     const textElement = document.createElement('div');
     textElement.classList.add('kg-callout-text');
-    textElement.innerHTML = node.calloutText;
+    
+    const dom = new JSDOM(node.calloutText);
+    const allowedTags = ['A', 'STRONG', 'EM'];
+    
+    const body = dom.window.document.body;
+    cleanDOM(body, allowedTags);
+    
+    textElement.innerHTML = body.innerHTML;
     element.appendChild(textElement);
 
     return {element};
+}
+
+function cleanDOM(node, allowedTags) {
+    for (let i = 0; i < node.childNodes.length; i++) {
+        let child = node.childNodes[i];
+        if (child.nodeType === 1 && !allowedTags.includes(child.tagName)) {
+            while (child.firstChild) {
+                node.insertBefore(child.firstChild, child);
+            }
+            node.removeChild(child);
+            i -= 1;
+        } else {
+            cleanDOM(child, allowedTags);
+        }
+    }
 }
