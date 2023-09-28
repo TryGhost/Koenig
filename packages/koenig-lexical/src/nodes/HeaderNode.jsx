30aa0f4113ed922fd773499b3c514d4f1e804c3f
Steve Larson <9larsons@gmail.com>
2023-09-28 08:37:23 -0500

    fixed logic

--- packages/koenig-lexical/src/nodes/HeaderNode.jsx
+++ packages/koenig-lexical/src/nodes/HeaderNode.jsx
@@ -33,7 +33,7 @@ export class HeaderNode extends BaseHeaderNode {
                 version: 1
             }),
             isHidden: ({config}) => {
-                return config?.deprecated?.headerV1 ? config.deprecated.headerV1 : true;
+                return config?.deprecated?.headerV1 ?? true;
             },
             shortcut: '/header'
         },
