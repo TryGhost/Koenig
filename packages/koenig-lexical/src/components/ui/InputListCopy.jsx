c531f5997d44b0ace982b70e4c219b4f97ac9a99
Peter Zimon <peter.zimon@gmail.com>
2024-06-06 14:35:28 +0200

    Position refinement

--- packages/koenig-lexical/src/components/ui/InputListCopy.jsx
+++ packages/koenig-lexical/src/components/ui/InputListCopy.jsx
@@ -84,7 +84,7 @@ export function InputListItem({dataTestId, item, selected, onClick, onMouseOver,
 
 export function InputListGroup({dataTestId, group}) {
     return (
-        <li className="mb-0 mt-2 flex items-center justify-between border-t border-grey-200 px-4 pb-2 pt-3 text-[1.1rem] font-semibold uppercase tracking-wide text-grey-600 first-of-type:mt-0 first-of-type:border-t-0 dark:border-grey-900" data-testid={`${dataTestId}-listGroup`}><div className="flex items-center gap-1.5"><Spinner size='mini' /> {group.label}</div></li>
+        <li className="mb-0 mt-2 flex items-center justify-between border-t border-grey-200 px-4 pb-2 pt-3 text-[1.1rem] font-semibold uppercase tracking-wide text-grey-600 first-of-type:mt-0 first-of-type:border-t-0 dark:border-grey-900" data-testid={`${dataTestId}-listGroup`}><div className="flex items-center gap-1.5"><span className="ml-px"><Spinner size='mini' /></span> {group.label}</div></li>
     );
 }
 
