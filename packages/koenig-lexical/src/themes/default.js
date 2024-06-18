14366a93057cdeb58196919fb388edc20ccc3586
Sanne de Vries <sannedv@protonmail.com>
2024-06-18 11:45:42 +0200

    Updated @-linking placeholder icon styles
    REF MOM-235

--- packages/koenig-lexical/src/themes/default.js
+++ packages/koenig-lexical/src/themes/default.js
@@ -31,9 +31,9 @@ const defaultTheme = {
     },
     code: undefined,
     tkHighlighted: 'bg-lime-500 dark:bg-lime-800 py-1',
-    atLink: 'inline-flex items-center bg-grey-200/70 mx-[-.2rem] px-1 pb-[.2rem] leading-[1.4] rounded dark:bg-grey-950',
+    atLink: 'inline-block bg-grey-200/70 mx-[-.2rem] px-1 pb-[.2rem] leading-[1.4] rounded dark:bg-grey-950',
     atLinkSearch: 'after:content-[attr(data-placeholder)] after:text-grey-500 dark:after:text-grey-600 min-w-[5px]',
-    atLinkIcon: 'px-1 text-grey-600 dark:text-grey-500'
+    atLinkIcon: 'inline-block mx-1 mb-[3px] text-grey-600 dark:text-grey-500'
 };
 
 export default defaultTheme;
