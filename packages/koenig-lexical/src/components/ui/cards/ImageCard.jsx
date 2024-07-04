TryGhost/Koenig

commit e0e183bc538da2467c347a56fe046cbc872bc293

Redesigned the toolbar (#1277)
* Redesigned the toolbar

REF MOM-238
- Changed to light toolbar
- Added tooltips including shortcut hints
- Updated icons from fill to stroke
- Moved pintura CTA out of the image toolbar onto image itself for consistency
- Added tooltips to all IconButtons

* Tweaked toolbar styles

REF MOM-238
- Updated border radiuses
- Updated shadow sizes
- Updated transition duration for toolbar
- Changed "Enhance" to "Edit" for Pintura buttons

* Added platform-specific symbols for keyboard shortcuts to toolbar

REF MOM-238
- Added utils to handle platform-specific symbols for keyboard shortcuts
- Updated toolbar to use these utils for displaying keyboard shortcuts

* Updated gallery and image card tests

REF MOM-238

* Hid Pintura button on gifs and fixed tests

REF MOM-238

* Removed test for image replace functionality and fixed broken tests

REF MOM-238

* Fixed tests

* Fixed remaining tests

* Added visibility toolbar button to html card behind flag

REF MOM-238

---------

Co-authored-by: Kevin Ansfield <kevin@lookingsideways.co.uk>

packages/koenig-lexical/src/components/ui/cards/ImageCard.jsx
@@ -1,12 +1,15 @@
 import ImageUploadForm from '../ImageUploadForm';
 import PropTypes from 'prop-types';
 import React from 'react';
+import WandIcon from '../../../assets/icons/kg-wand.svg?react';
 import {CardCaptionEditor} from '../CardCaptionEditor';
 import {CardText, MediaPlaceholder} from '../MediaPlaceholder';
+import {IconButton} from '../IconButton';
 import {ProgressBar} from '../ProgressBar';
+import {isGif} from '../../../utils/isGif';
 import {openFileSelection} from '../../../utils/openFileSelection';
 
-function PopulatedImageCard({src, alt, previewSrc, imageUploader, imageCardDragHandler, imageFileDragHandler}) {
+function PopulatedImageCard({src, alt, previewSrc, imageUploader, imageCardDragHandler, imageFileDragHandler, isPinturaEnabled, openImageEditor, onFileChange}) {
     const progressStyle = {
         width: `${imageUploader.progress?.toFixed(0)}%`
     };
@@ -19,7 +22,7 @@ function PopulatedImageCard({src, alt, previewSrc, imageUploader, imageCardDragH
     }
 
     return (
-        <div ref={setRef} className="not-kg-prose relative">
+        <div ref={setRef} className="not-kg-prose group/image relative">
             <img
                 alt={alt ? alt : progressAlt}
                 className={`mx-auto block ${previewSrc ? 'opacity-40' : ''}`}
@@ -42,6 +45,22 @@ function PopulatedImageCard({src, alt, previewSrc, imageUploader, imageCardDragH
                     <CardText text="Drop to replace image" />
                 </div>
             ) : null}
+            {(isPinturaEnabled && !isGif(src)) &&
+                <div className={`pointer-events-none invisible absolute inset-0 bg-gradient-to-t from-black/0 via-black/5 to-black/30 p-3 opacity-0 transition-all group-hover/image:visible group-hover/image:opacity-100`}>
+                    <div className="flex flex-row-reverse">
+                        <IconButton Icon={WandIcon} label="Edit" onClick={() => openImageEditor({
+                            image: src,
+                            handleSave: (editedImage) => {
+                                onFileChange({
+                                    target: {
+                                        files: [editedImage]
+                                    }
+                                });
+                            }
+                        })} />
+                    </div>
+                </div>
+            }
         </div>
     );
 }
@@ -81,7 +100,9 @@ const ImageHolder = ({
     onFileChange,
     setFileInputRef,
     imageCardDragHandler,
-    imageFileDragHandler
+    imageFileDragHandler,
+    isPinturaEnabled,
+    openImageEditor
 }) => {
     if (previewSrc || src) {
         return (
@@ -90,6 +111,8 @@ const ImageHolder = ({
                 imageCardDragHandler={imageCardDragHandler}
                 imageFileDragHandler={imageFileDragHandler}
                 imageUploader={imageUploader}
+                isPinturaEnabled={isPinturaEnabled}
+                openImageEditor={openImageEditor}
                 previewSrc={previewSrc}
                 src={src}
             />
@@ -120,7 +143,9 @@ export function ImageCard({
     previewSrc,
     imageUploader,
     imageCardDragHandler,
-    imageFileDragHandler
+    imageFileDragHandler,
+    isPinturaEnabled,
+    openImageEditor
 }) {
     const figureRef = React.useRef(null);
 
@@ -143,6 +168,8 @@ export function ImageCard({
                     imageCardDragHandler={imageCardDragHandler}
                     imageFileDragHandler={imageFileDragHandler}
                     imageUploader={imageUploader}
+                    isPinturaEnabled={isPinturaEnabled}
+                    openImageEditor={openImageEditor}
                     previewSrc={previewSrc}
                     setFileInputRef={setFileInputRef}
                     src={src}
@@ -172,7 +199,9 @@ ImageHolder.propTypes = {
     onFileChange: PropTypes.func,
     setFileInputRef: PropTypes.func,
     imageFileDragHandler: PropTypes.object,
-    imageCardDragHandler: PropTypes.object
+    imageCardDragHandler: PropTypes.object,
+    isPinturaEnabled: PropTypes.bool,
+    openImageEditor: PropTypes.func
 };
 
 PopulatedImageCard.propTypes = {
@@ -180,8 +209,11 @@ PopulatedImageCard.propTypes = {
     alt: PropTypes.string,
     previewSrc: PropTypes.string,
     imageUploader: PropTypes.object,
+    onFileChange: PropTypes.func,
     imageCardDragHandler: PropTypes.object,
-    imageFileDragHandler: PropTypes.object
+    imageFileDragHandler: PropTypes.object,
+    isPinturaEnabled: PropTypes.bool,
+    openImageEditor: PropTypes.func
 };
 
 EmptyImageCard.propTypes = {
@@ -205,5 +237,7 @@ ImageCard.propTypes = {
     previewSrc: PropTypes.string,
     imageUploader: PropTypes.object,
     imageFileDragHandler: PropTypes.object,
-    imageCardDragHandler: PropTypes.object
+    imageCardDragHandler: PropTypes.object,
+    isPinturaEnabled: PropTypes.bool,
+    openImageEditor: PropTypes.func
 };

commit 2914e707ce07d47d42466e35fada7e1f2e626e73

Fixed image editing not saving changes
refs [ENG-1363](https://linear.app/tryghost/issue/ENG-1363/bug-with-image-editing-not-saving-changes)

Fixed image editing not saving changes due to `onFileChange` handler erroneously
not being passed to the `PopulatedImageCard` component. This is regression was
introduced in: https://github.com/TryGhost/Koenig/pull/1277

packages/koenig-lexical/src/components/ui/cards/ImageCard.jsx
@@ -115,6 +115,7 @@ const ImageHolder = ({
                 openImageEditor={openImageEditor}
                 previewSrc={previewSrc}
                 src={src}
+                onFileChange={onFileChange}
             />
         );
     } else {
@@ -209,11 +210,11 @@ PopulatedImageCard.propTypes = {
     alt: PropTypes.string,
     previewSrc: PropTypes.string,
     imageUploader: PropTypes.object,
-    onFileChange: PropTypes.func,
     imageCardDragHandler: PropTypes.object,
     imageFileDragHandler: PropTypes.object,
     isPinturaEnabled: PropTypes.bool,
-    openImageEditor: PropTypes.func
+    openImageEditor: PropTypes.func,
+    onFileChange: PropTypes.func
 };
 
 EmptyImageCard.propTypes = {