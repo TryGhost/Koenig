d882e134799d13c17760ad45da26178709c4c003
Sanne de Vries <sannedv@protonmail.com>
2024-07-25 14:36:35 +0200

    Updated to more intuitive UI of the visibility dropdown
    REF MOM-326

--- packages/koenig-lexical/src/components/ui/VisibilityDropdown.jsx
+++ packages/koenig-lexical/src/components/ui/VisibilityDropdown.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Dropdown} from './Dropdown';
 import {Toggle} from './Toggle';
 import {useVisibilityToggle} from '../../hooks/useVisibilityToggle.js';
 
 export function VisibilityDropdown({editor, nodeKey, visibility, isActive}) {
-    const [emailVisibility, toggleEmail, toggleMembers, freeMemberVisibility, paidMemberVisibility] = useVisibilityToggle(editor, nodeKey, visibility);
+    const [emailVisibility, toggleEmail] = useVisibilityToggle(editor, nodeKey, visibility);
+
+    const dropdownOptions = [{
+        label: 'All subscribers',
+        name: 'status:all'
+    }, {
+        label: 'Free subscribers',
+        name: 'status:free'
+    }, {
+        label: 'Paid subscribers',
+        name: 'status:-free'
+    }];
 
     if (!isActive) {
         return <></>;
@@ -11,28 +23,25 @@ export function VisibilityDropdown({editor, nodeKey, visibility, isActive}) {
 
     if (isActive) {
         return (
-            <div className="absolute left-1/2 top-0 z-[1001] flex w-[254px] -translate-x-1/2 flex-col gap-1 rounded-lg bg-white p-6 shadow-md">
+            <div className="not-kg-prose absolute left-1/2 top-0 z-[1001] flex w-[254px] -translate-x-1/2 flex-col gap-1 rounded-lg bg-white p-6 shadow-md">
                 <div className="text-sm font-bold">Visibility</div>
                 <ToggleSetting
                     dataTestId='visibility-toggle-email-only'
                     isChecked={emailVisibility}
-                    label="Show in email only"
+                    label="Show on web"
+                    onChange={e => toggleEmail(e)} />
+                <hr className="mt-1 border-grey-250 pb-1 dark:border-white/5" />
+                <ToggleSetting
+                    dataTestId='visibility-toggle-email-only'
+                    isChecked={emailVisibility}
+                    label="Show in email"
                     onChange={e => toggleEmail(e)} />
                 {
                     emailVisibility && (
                         <>
-                            <hr className="mt-1 border-grey-250 pb-1 dark:border-white/5" />
-                            <ToggleSetting
-                                dataTestId='visibility-toggle-free-members'
-                                isChecked={freeMemberVisibility}
-                                label="Free members"
-                                onChange={e => toggleMembers(e, 'free')}
-                            />
-                            <ToggleSetting
-                                dataTestId='visibility-toggle-paid-members'
-                                isChecked={paidMemberVisibility}
-                                label="Paid members"
-                                onChange={e => toggleMembers(e, 'paid')}
+                            <Dropdown
+                                menu={dropdownOptions}
+                                value='All subscribers'
                             />
                         </>
                     )
