d6a31961ab7dfdd7cc804b70e7cbb1ae6b04c427
Sag <guptazy@gmail.com>
2024-08-12 15:17:29 +0200

    Removed extra paragraphs around headings and lists  when pasting from Gdoc

--- packages/kg-default-nodes/lib/serializers/linebreak.js
+++ packages/kg-default-nodes/lib/serializers/linebreak.js
@@ -3,17 +3,26 @@ export default {
         br: (node) => {
             const isGoogleDocs = !!node.closest('[id^="docs-internal-guid-"]');
 
-            // render nothing for GDoc line breaks in-between paragraphs
-            // otherwise we end up with empty paragraphs
-            if (
-                isGoogleDocs &&
-                node.previousElementSibling?.nodeName === 'P' &&
-                node.nextElementSibling?.nodeName === 'P'
-            ) {
-                return {
-                    conversion: () => null,
-                    priority: 1
-                };
+            // Remove empty paragraphs when copy/pasting from Google docs:
+            // - Between two paragraphs (P and P)
+            // - Between a list and a paragraph (UL/OL and P), and vice versa
+            // - Between a heading and a paragraph (H1-H6 and P), and vice versa
+            if (isGoogleDocs) {
+                if ((
+                    node.previousElementSibling?.nodeName === 'P' &&
+                    node.nextElementSibling?.nodeName === 'P'
+                ) || (
+                    ['UL', 'OL', 'DL', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(node.previousElementSibling?.nodeName) &&
+                    node.nextElementSibling?.nodeName === 'P'
+                ) || (
+                    node.previousElementSibling?.nodeName === 'P' &&
+                    ['UL', 'OL', 'DL', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(node.nextElementSibling?.nodeName)
+                )) {
+                    return {
+                        conversion: () => null,
+                        priority: 1
+                    };
+                }
             }
 
             // allow lower priority converter to handle (i.e. default LineBreakNode.importDOM)
