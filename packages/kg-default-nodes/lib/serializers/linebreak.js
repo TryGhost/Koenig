84ed25f1609d4976d57301be2a2c8fede9ee783a
Sag <guptazy@gmail.com>
2024-08-13 10:31:01 +0200

    Improved readability

--- packages/kg-default-nodes/lib/serializers/linebreak.js
+++ packages/kg-default-nodes/lib/serializers/linebreak.js
@@ -2,21 +2,23 @@ export default {
     import: {
         br: (node) => {
             const isGoogleDocs = !!node.closest('[id^="docs-internal-guid-"]');
+            const headings = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+            const lists = ['UL', 'OL', 'DL'];
 
             // Remove empty paragraphs when copy/pasting from Google docs:
             // - Between two paragraphs (P and P)
-            // - Between a list and a paragraph (UL/OL and P), and vice versa
+            // - Between a list and a paragraph (UL/OL/DL and P), and vice versa
             // - Between a heading and a paragraph (H1-H6 and P), and vice versa
             if (isGoogleDocs) {
                 if ((
                     node.previousElementSibling?.nodeName === 'P' &&
                     node.nextElementSibling?.nodeName === 'P'
                 ) || (
-                    ['UL', 'OL', 'DL', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(node.previousElementSibling?.nodeName) &&
+                    [...headings, ...lists].includes(node.previousElementSibling?.nodeName) &&
                     node.nextElementSibling?.nodeName === 'P'
                 ) || (
                     node.previousElementSibling?.nodeName === 'P' &&
-                    ['UL', 'OL', 'DL', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].includes(node.nextElementSibling?.nodeName)
+                    [...headings, ...lists].includes(node.nextElementSibling?.nodeName)
                 )) {
                     return {
                         conversion: () => null,
