bc623accebc0c8b16417121d6ba91f142e4919a1
Sanne de Vries <sannedv@protonmail.com>
2024-09-04 14:25:17 +0200

    Moved HTML card visibility settings to the SettingsPanel
    REF PLG-206
    - Moved HTML card visibility settings to the SettingsPanel
    - Added TabView component
    - Removed VisibilityDropdown component

---
+++ packages/koenig-lexical/src/components/ui/TabView.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React, {useState} from 'react';
+
+const TabView = ({tabs, defaultTab, tabContent}) => {
+    const [activeTab, setActiveTab] = useState(defaultTab || tabs[0].id);
+
+    const handleTabChange = (tabId) => {
+        setActiveTab(tabId);
+    };
+
+    return (
+        <>
+            <div className="no-scrollbar flex w-full gap-5 overflow-x-auto border-b border-grey-300 dark:border-grey-900">
+                {tabs.map(tab => (
+                    <button
+                        key={tab.id}
+                        className={`-m-b-px cursor-pointer appearance-none whitespace-nowrap border-b-2 pb-1 text-sm transition-all ${
+                            activeTab === tab.id
+                                ? 'border-black font-bold text-black dark:border-white dark:text-white'
+                                : 'border-transparent font-medium tracking-[.022em] text-grey-600 hover:border-grey-500 dark:text-white'
+                        }`}
+                        type="button"
+                        onClick={() => handleTabChange(tab.id)}
+                    >
+                        {tab.label}
+                    </button>
+                ))}
+            </div>
+            {tabContent[activeTab]}
+        </>
+    );
+};
+
+TabView.propTypes = {
+    tabs: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    defaultTab: PropTypes.string,
+    tabContent: PropTypes.objectOf(PropTypes.node).isRequired
+};
+
+export {TabView};
