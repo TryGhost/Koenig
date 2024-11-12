84fcf585462ffc9d1c869b51795d1e9f84b7c9de
Sag <guptazy@gmail.com>
2024-11-12 15:05:12 +0800

    🔒 Escaped HTML from bookmark fields
    ref https://linear.app/ghost/issue/ENG-1751
    
    - in order to prevent potential cross-site scripting (XSS) vulnerabilities or HTML injection, we are now escaping bookmark fields for email
    - bookmark fields were already escaped for web, by using textContent

--- packages/kg-default-nodes/lib/nodes/bookmark/bookmark-renderer.js
+++ packages/kg-default-nodes/lib/nodes/bookmark/bookmark-renderer.js
@@ -1,5 +1,6 @@
 import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 import {renderEmptyContainer} from '../../utils/render-empty-container';
+import {escapeHtml} from '../../utils/escape-html';
 
 export function renderBookmarkNode(node, options = {}) {
     addCreateDocumentOption(options);
@@ -62,18 +63,18 @@ function truncateHtml(text, maxLength, maxLengthMobile) {
 }
 
 function emailTemplate(node, document) {
-    const title = node.title;
-    const publisher = node.publisher;
-    const author = node.author;
-    const icon = node.icon;
-    const description = node.description;
-    const url = node.url;
-    const thumbnail = node.thumbnail;
-    const caption = node.caption;
+    const title = escapeHtml(node.title);
+    const publisher = escapeHtml(node.publisher);
+    const author = escapeHtml(node.author);
+    const icon = escapeHtml(node.icon);
+    const description = escapeHtml(node.description);
+    const url = escapeHtml(node.url);
+    const thumbnail = escapeHtml(node.thumbnail);
+    const caption = escapeHtml(node.caption);
 
     const element = document.createElement('div');
 
-    const html = 
+    const html =
         `
         <!--[if !mso !vml]-->
             <figure class="kg-card kg-bookmark-card ${caption ? `kg-card-hascaption` : ''}">
