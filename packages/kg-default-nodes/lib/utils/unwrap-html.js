933063aa2da22965e990479d7dfd5d6e51e717a5
Cathy Sarisky <catsarisky@gmail.com>
2024-11-30 16:17:06 -0500

    improve coverage

--- packages/kg-default-nodes/lib/utils/unwrap-html.js
+++ packages/kg-default-nodes/lib/utils/unwrap-html.js
@@ -1,8 +1,16 @@
-
 function unwrapHtml(html) {
+    /* Faster but confused by two adjacent p tags.
     if (/^<p(\s+[^>]*)?>([\s\S]*)<\/p>$/i.test(html.trim())) {
         console.log('INFO: strip extra paragraph tag from product title');
         return html.replace(/^<p(\s+[^>]*)?>([\s\S]*)<\/p>$/i, '$2');
+    }
+    return html;
+    */
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    // if the div has a single child, and that child is a paragraph, we can safely remove the paragraph.
+    if (div.childNodes.length === 1 && div.childNodes[0].nodeName === 'P') {
+        return div.childNodes[0].innerHTML;
     }
     return html;
 }
--- packages/kg-default-nodes/test/unwrap-html.test.js
+++ packages/kg-default-nodes/test/unwrap-html.test.js
@@ -1,30 +1,41 @@
 const chai = require('chai');
 const expect = chai.expect;
 const unwrapHtml = require("../lib/utils/unwrap-html");
+require('./utils');
+
+const {JSDOM} = require('jsdom');
+const DOMParser = require('@tryghost/mobiledoc-kit/dist/commonjs/mobiledoc-kit/parsers/dom').default;
+
+const buildDOM = function () {
+    return (new JSDOM(`<!DOCTYPE html><html><body></body></html>`)).window.document;
+};
 
 describe('unwrapHtml', () => {
+    before(() => {
+        global.document = buildDOM();
+    })
     it('should remove wrapping p tags', () => {
         const html = '<p>Test</p>';
-        expect(unwrapHtml(html)).toBe('Test');
+        expect(unwrapHtml(html)).equal('Test');
     });
     it('should not remove other tags', () => {
         const html = '<span>Test</span>';
-        expect(unwrapHtml(html)).toBe('<span>Test</span>');
+        expect(unwrapHtml(html)).equal('<span>Test</span>');
     });
     it('should remove wrapping p tags with attributes', () => {
         const html = '<p class="test" id="something">Test</p>';
-        expect(unwrapHtml(html)).toBe('Test');
+        expect(unwrapHtml(html)).equal('Test');
     });
     it('should not remove p tags that are not wrapping', () => {
         const html = '<p>Test</p><p>Test</p>';
-        expect(unwrapHtml(html)).toBe('<p>Test</p><p>Test</p>');
+        expect(unwrapHtml(html)).equal('<p>Test</p><p>Test</p>');
     });
     it('should not remove p tags that are not wrapping with attributes', () => {
         const html = '<p class="test" id="something">Test</p><p class="test" id="something">Test</p>';
-        expect(unwrapHtml(html)).toBe('<p class="test" id="something">Test</p><p class="test" id="something">Test</p>');
+        expect(unwrapHtml(html)).equal('<p class="test" id="something">Test</p><p class="test" id="something">Test</p>');
     });
     it('should not remove p tags if not outermost', () => {
         const html = '<div><p>Test</p></div>';
-        expect(unwrapHtml(html)).toBe('<div><p>Test</p></div>');
+        expect(unwrapHtml(html)).equal('<div><p>Test</p></div>');
     });
 });
