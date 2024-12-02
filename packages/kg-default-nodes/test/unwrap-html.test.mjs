60edb1f7c63c93c50b9be6bb3a7b09207793348d
Cathy Sarisky <catsarisky@gmail.com>
2024-12-02 09:56:52 -0500

    fix missed test.

--- packages/kg-default-nodes/test/unwrap-html.test.mjs
+++ packages/kg-default-nodes/test/unwrap-html.test.mjs
@@ -1,41 +1,39 @@
-const chai = require('chai');
-const expect = chai.expect;
+import {expect} from 'chai';
 import unwrapHtml from '../lib/utils/unwrap-html.mjs';
-require('./utils');
-
-const {JSDOM} = require('jsdom');
-const DOMParser = require('@tryghost/mobiledoc-kit/dist/commonjs/mobiledoc-kit/parsers/dom').default;
+import {JSDOM} from 'jsdom';
+import {beforeEach} from 'mocha';
 
 const buildDOM = function () {
-    return (new JSDOM(`<!DOCTYPE html><html><body></body></html>`)).window.document;
+    return (new JSDOM()).window.document;
 };
 
 describe('unwrapHtml', () => {
-    before(() => {
-        global.document = buildDOM();
-    })
+    let dom;
+    beforeEach(() => {
+        dom = buildDOM();
+    });
     it('should remove wrapping p tags', () => {
         const html = '<p>Test</p>';
-        expect(unwrapHtml(html)).equal('Test');
+        expect(unwrapHtml(html, dom)).equal('Test');
     });
     it('should not remove other tags', () => {
         const html = '<span>Test</span>';
-        expect(unwrapHtml(html)).equal('<span>Test</span>');
+        expect(unwrapHtml(html, dom)).equal('<span>Test</span>');
     });
     it('should remove wrapping p tags with attributes', () => {
         const html = '<p class="test" id="something">Test</p>';
-        expect(unwrapHtml(html)).equal('Test');
+        expect(unwrapHtml(html, dom)).equal('Test');
     });
     it('should not remove p tags that are not wrapping', () => {
         const html = '<p>Test</p><p>Test</p>';
-        expect(unwrapHtml(html)).equal('<p>Test</p><p>Test</p>');
+        expect(unwrapHtml(html, dom)).equal('<p>Test</p><p>Test</p>');
     });
     it('should not remove p tags that are not wrapping with attributes', () => {
         const html = '<p class="test" id="something">Test</p><p class="test" id="something">Test</p>';
-        expect(unwrapHtml(html)).equal('<p class="test" id="something">Test</p><p class="test" id="something">Test</p>');
+        expect(unwrapHtml(html, dom)).equal('<p class="test" id="something">Test</p><p class="test" id="something">Test</p>');
     });
     it('should not remove p tags if not outermost', () => {
         const html = '<div><p>Test</p></div>';
-        expect(unwrapHtml(html)).equal('<div><p>Test</p></div>');
+        expect(unwrapHtml(html, dom)).equal('<div><p>Test</p></div>');
     });
 });
