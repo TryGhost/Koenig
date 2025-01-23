193d7cd86e9166765ad7767266da9aee2154f9b3
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-01-23 16:36:36 +0900

    Added URL input handling on CTA Card
    ref https://linear.app/ghost/issue/PLG-313/implement-button-url-update
    
    - Added URL Input handling for the new CTA Card
    - Allows URL input or selecting from a populated list
    - Added additional test to ensure the button persists in the UI when
      conditions are met, which are whether the URL field is populated, Text
    is populated and the button is active.

--- packages/koenig-lexical/test/e2e/cards/cta-card.test.js
+++ packages/koenig-lexical/test/e2e/cards/cta-card.test.js
@@ -68,11 +68,63 @@ test.describe('Call To Action Card', async () => {
         await focusEditor(page);
         await insertCard(page, {cardName: 'call-to-action'});
         await page.click('[data-testid="button-settings"]');
-        await page.fill('[data-testid="button-url"]', 'https://example.com');
-        // Check that the `data-test-cta-button-current-url` attribute is set correctly
+        await page.fill('[data-testid="button-url"]', 'https://example.com/somepost');
         const buttonContainer = await page.$('[data-test-cta-button-current-url]');
         const currentUrl = await buttonContainer.getAttribute('data-test-cta-button-current-url');
-        // Assert the value
-        expect(currentUrl).toBe('https://example.com');
+        expect(currentUrl).toBe('https://example.com/somepost');
+    });
+
+    // NOTE: an improvement would be to pass in suggested url options, but the construction now doesn't make that straightforward
+    test('suggested urls display', async function () {
+        await focusEditor(page);
+        await insertCard(page, {cardName: 'call-to-action'});
+        await page.click('[data-testid="button-settings"]');
+
+        const buttonTextInput = await page.getByTestId('button-url');
+        await expect(buttonTextInput).toHaveValue('');
+
+        // this is dependent on the test values inserted in the node
+        await page.getByTestId('button-url').fill('Ho');
+        // // this is too fast, need to try two inputs or add a delay before checking the suggested options
+        await page.getByTestId('button-url').fill('me');
+        await expect(await page.getByTestId('button-url-listOption')).toContainText('Homepage');
+        // click on homepage
+        await page.getByTestId('button-url-listOption').click();
+        // check if the url is set
+        const buttonContainer = await page.$('[data-test-cta-button-current-url]');
+        const currentUrl = await buttonContainer.getAttribute('data-test-cta-button-current-url');
+        // current view can be any url, so check for a valid url
+        const validUrlRegex = /^(https?:\/\/)([\w.-]+)(:[0-9]+)?(\/[\w.-]*)*(\?.*)?(#.*)?$/;
+        // Assert the URL is valid
+        expect(currentUrl).toMatch(validUrlRegex);
+    });
+
+    test('button doesnt disappear when toggled, has text, has url and loses focus', async function () {
+        await focusEditor(page);
+        await insertCard(page, {cardName: 'call-to-action'});
+        await page.click('[data-testid="button-settings"]');
+        await page.fill('[data-testid="button-text"]', 'Click me');
+        await page.fill('[data-testid="button-url"]', 'https://example.com/somepost');
+        expect(await page.isVisible('[data-testid="cta-button"]')).toBe(true);
+        expect(await page.textContent('[data-testid="cta-button"]')).toBe('Click me');
+        const buttonContainer = await page.$('[data-test-cta-button-current-url]');
+        const currentUrl = await buttonContainer.getAttribute('data-test-cta-button-current-url');
+        expect(currentUrl).toBe('https://example.com/somepost');
+
+        // lose focus and editing mode
+        await page.keyboard.press('Escape');
+        await page.keyboard.press('Enter');
+
+        // check if editing is false
+        await assertHTML(page, html`
+            <div data-lexical-decorator="true" contenteditable="false">
+                <div data-kg-card-editing="false" data-kg-card-selected="false" data-kg-card="call-to-action">
+                </div>
+            </div>
+            <p><br /></p>
+            <p><br /></p>
+        `, {ignoreCardContents: true});
+
+        expect(await page.isVisible('[data-testid="cta-button"]')).toBe(true);
     });
 });
