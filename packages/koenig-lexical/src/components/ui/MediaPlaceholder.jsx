3ea83f263887735fd1972473d78e32ff577e5767
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-02-25 18:10:03 +0000

    Cleaned up repetition in `<MediaPlaceholder>`
    - large parts of the component were repeated in order to surround varying contents
    - extracted contents to separate children to make behaviour clearer
    - improved classes assignment to remove duplication of the button's class list

--- packages/koenig-lexical/src/components/ui/MediaPlaceholder.jsx
+++ packages/koenig-lexical/src/components/ui/MediaPlaceholder.jsx
@@ -18,7 +18,7 @@ export const PLACEHOLDER_ICONS = {
 };
 
 export const CardText = ({text, type}) => (
-    <span 
+    <span
         className={clsx(
             'text-center font-sans text-sm font-semibold text-grey-800 transition-all group-hover:text-grey-800',
             type === 'button' && 'px-3 py-1'
@@ -29,31 +29,19 @@ export const CardText = ({text, type}) => (
     </span>
 );
 
-export function MediaPlaceholder({
-    desc,
-    icon,
-    filePicker,
-    size,
-    type,
-    borderStyle,
-    isDraggedOver,
-    errors = [],
-    placeholderRef,
-    dataTestId = 'media-placeholder',
-    errorDataTestId = 'media-placeholder-errors',
-    multiple = false,
-    ...props
-}) {
-    const Icon = PLACEHOLDER_ICONS[icon];
+const ButtonContents = ({desc, hasErrors}) => {
+    if (hasErrors) {
+        return;
+    }
+    return <p className="!font-sans !text-[1.3rem] !font-medium text-grey-900">{desc}</p>;
+};
 
-    const containerClasses = clsx(
-        'relative flex h-full items-center justify-center',
-        type === 'button' ? 'rounded-lg bg-grey-100' : 'border bg-grey-50',
-        size === 'xsmall' && type !== 'button' && 'before:pb-[12.5%] dark:bg-grey-900',
-        size !== 'xsmall' && type !== 'button' && 'before:pb-[62.5%] dark:bg-grey-950',
-        borderStyle === 'rounded' && type !== 'button' && 'rounded-lg border-grey/20 dark:border-transparent',
-        borderStyle !== 'rounded' && type !== 'button' && 'border-grey/20 dark:border-grey/10'
-    );
+const StandardContents = ({desc, hasErrors, icon, size}) => {
+    if (size === 'xsmall' && hasErrors) {
+        return;
+    }
+
+    const Icon = PLACEHOLDER_ICONS[icon];
 
     const iconClasses = clsx(
         'shrink-0 opacity-80 transition-all ease-linear hover:scale-105 group-hover:opacity-100',
@@ -70,16 +58,56 @@ export function MediaPlaceholder({
         size !== 'xsmall' && '!mt-4'
     );
 
+    return <>
+        <Icon className={iconClasses} />
+        <p className={descriptionClasses}>{desc}</p>
+    </>;
+};
+
+export function MediaPlaceholder({
+    desc,
+    icon,
+    filePicker,
+    size,
+    type,
+    borderStyle = 'squared',
+    isDraggedOver,
+    errors = [],
+    placeholderRef,
+    dataTestId = 'media-placeholder',
+    errorDataTestId = 'media-placeholder-errors',
+    multiple = false,
+    ...props
+}) {
+    const containerClasses = clsx(
+        'relative flex h-full items-center justify-center',
+        type === 'button' ? 'rounded-lg bg-grey-100' : 'border bg-grey-50',
+        size === 'xsmall' && type !== 'button' && 'before:pb-[12.5%] dark:bg-grey-900',
+        size !== 'xsmall' && type !== 'button' && 'before:pb-[62.5%] dark:bg-grey-950',
+        borderStyle === 'rounded' && type !== 'button' && 'rounded-lg border-grey/20 dark:border-transparent',
+        borderStyle !== 'rounded' && type !== 'button' && 'border-grey/20 dark:border-grey/10'
+    );
+
     const buttonClasses = clsx(
         'group flex cursor-pointer select-none items-center justify-center',
-        size === 'xsmall' && 'p-4',
-        size !== 'xsmall' && 'flex-col p-20'
+        type === 'button' && 'px-3 py-1',
+        type !== 'button' && (size === 'xsmall' ? 'p-4' : 'flex-col p-20')
     );
 
     const errorClasses = clsx(
         'font-sans text-sm font-semibold text-red',
         size !== 'xsmall' && 'mt-3 max-w-[65%]'
     );
+
+    const errorMessages = errors.map(error => (
+        <span
+            key={error.message}
+            className={errorClasses}
+            data-testid={errorDataTestId}
+        >
+            {error.message}
+        </span>
+    ));
 
     return (
         <div
@@ -92,52 +120,19 @@ export function MediaPlaceholder({
                 {isDraggedOver ? (
                     <CardText text={`Drop ${multiple ? '\'em' : 'it'} like it's hot 🔥`} type={type} />
                 ) : (
-                    <>
-                        {type === 'button' ? (
-                            <button 
-                                className="group flex cursor-pointer select-none items-center justify-center px-3 py-1" 
-                                name="placeholder-button" 
-                                type="button" 
-                                onClick={filePicker}
-                            >
-                                {!errors.length && (
-                                    <p className="!font-sans !text-[1.3rem] !font-medium text-grey-900">{desc}</p>
-                                )}
-                                {errors.map(error => (
-                                    <span
-                                        key={error.message}
-                                        className={errorClasses}
-                                        data-testid={errorDataTestId}
-                                    >
-                                        {error.message}
-                                    </span>
-                                ))}
-                            </button>
-                        ) : (
-                            <button 
-                                className={buttonClasses}
-                                name="placeholder-button" 
-                                type="button" 
-                                onClick={filePicker}
-                            >
-                                {!(size === 'xsmall' && errors.length > 0) && (
-                                    <>
-                                        <Icon className={iconClasses} />
-                                        <p className={descriptionClasses}>{desc}</p>
-                                    </>
-                                )}
-                                {errors.map(error => (
-                                    <span
-                                        key={error.message}
-                                        className={errorClasses}
-                                        data-testid={errorDataTestId}
-                                    >
-                                        {error.message}
-                                    </span>
-                                ))}
-                            </button>
-                        )}
-                    </>
+                    <button
+                        className={buttonClasses}
+                        name="placeholder-button"
+                        type="button"
+                        onClick={filePicker}
+                    >
+                        {type === 'button'
+                            ? <ButtonContents desc={desc} hasErrors={errors.length > 0} />
+                            : <StandardContents desc={desc} hasErrors={errors.length > 0} icon={icon} size={size} />
+                        }
+
+                        {errorMessages}
+                    </button>
                 )}
             </div>
         </div>
