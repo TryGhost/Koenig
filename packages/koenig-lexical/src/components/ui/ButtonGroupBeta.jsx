44e0a2989f47b566f331154d4724193fca8f26c2
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-03-03 11:21:15 +0000

    added roles and aria-checked to ButtonGroupBeta elements

--- packages/koenig-lexical/src/components/ui/ButtonGroupBeta.jsx
+++ packages/koenig-lexical/src/components/ui/ButtonGroupBeta.jsx
@@ -7,7 +7,7 @@ import {usePreviousFocus} from '../../hooks/usePreviousFocus';
 export function ButtonGroupBeta({buttons = [], selectedName, onClick, hasTooltip = true}) {
     return (
         <div className="flex">
-            <ul className="flex items-center justify-evenly rounded-lg bg-grey-100 font-sans text-md font-normal text-white">
+            <ul className="flex items-center justify-evenly rounded-lg bg-grey-100 font-sans text-md font-normal text-white" role="menubar">
                 {buttons.map(({label, name, Icon, dataTestId, ariaLabel}) => (
                     <ButtonGroupIconButton
                         key={`${name}-${label}`}
@@ -34,9 +34,11 @@ export function ButtonGroupIconButton({dataTestId, onClick, label, ariaLabel, na
     return (
         <li className="mb-0">
             <button
+                aria-checked={isActive}
                 aria-label={ariaLabel || label}
                 className={`group relative flex h-7 w-8 cursor-pointer items-center justify-center rounded-lg text-black dark:text-white dark:hover:bg-grey-900 ${isActive ? 'border border-grey-300 bg-white shadow-xs dark:bg-grey-900' : '' } ${Icon ? '' : 'text-[1.3rem] font-bold'}`}
                 data-testid={dataTestId}
+                role="menuitemradio"
                 type="button"
                 onClick={handleClick}
                 onMouseDown={handleMousedown}
@@ -54,8 +56,8 @@ ButtonGroupBeta.propTypes = {
     onClick: PropTypes.func.isRequired,
     buttons: PropTypes.arrayOf(PropTypes.shape({
         label: PropTypes.string,
-        name: PropTypes.string,
-        Icon: PropTypes.func,
+        name: PropTypes.string.isRequired,
+        Icon: PropTypes.elementType,
         dataTestId: PropTypes.string,
         ariaLabel: PropTypes.string
     }))
