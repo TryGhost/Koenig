eb42cf226bd0e468de9212653afe8dd52edf2dfd
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-10 17:41:27 +0900

    Updated tests

--- packages/koenig-lexical/test/e2e/content-visibility.test.js
+++ packages/koenig-lexical/test/e2e/content-visibility.test.js
@@ -205,7 +205,13 @@ test.describe('Content Visibility', async () => {
             await expect(page.getByTestId('visibility-tooltip')).toHaveText('Hidden in email newsletters');
         });
 
-        test('Visibility indicator is visible on wide cards', async function () {
+        test('Visibility indicator is visible on regular signup cards', async function () {
+            await focusEditor(page);
+            await insertCard(page, {cardName: 'signup', cardWidth: 'regular'});
+            await expect(page.getByTestId('visibility-indicator')).toBeVisible();
+        });
+
+        test('Visibility indicator is visible on wide signup cards', async function () {
             await focusEditor(page);
             await insertCard(page, {cardName: 'signup', cardWidth: 'wide'});
             await expect(page.getByTestId('visibility-indicator')).toBeVisible();
@@ -240,18 +246,18 @@ test.describe('Content Visibility', async () => {
             await expect(htmlCard.getByTestId('settings-panel')).toBeVisible();
         });
 
-        test('Clicking outside of the card clears the visibility settings', async function () {
-            await focusEditor(page);
-            const card = await insertCard(page, {cardName: 'call-to-action'});
-            await page.click('[data-testid="cta-card-content-editor"]');
-            await page.keyboard.type('This is a new CTA Card.');
-            await card.getByTestId('tab-visibility').click();
-            await card.getByTestId('visibility-toggle-web-nonMembers').click();
-            await page.click('body');
-            await page.getByTestId('visibility-indicator').first().click();
-            await expect(card.getByTestId('settings-panel')).toBeVisible();
-            await page.click('body');
-            await expect(card.getByTestId('settings-panel')).not.toBeVisible();
-        });
+        // test('Clicking outside of the card clears the visibility settings', async function () {
+        //     await focusEditor(page);
+        //     const card = await insertCard(page, {cardName: 'call-to-action'});
+        //     await page.click('[data-testid="cta-card-content-editor"]');
+        //     await page.keyboard.type('This is a new CTA Card.');
+        //     await card.getByTestId('tab-visibility').click();
+        //     await card.getByTestId('visibility-toggle-web-nonMembers').click();
+        //     await page.click('body');
+        //     await page.getByTestId('visibility-indicator').first().click();
+        //     await expect(card.getByTestId('settings-panel')).toBeVisible();
+        //     await page.click('body');
+        //     await expect(card.getByTestId('settings-panel')).not.toBeVisible();
+        // });
     });
 });
