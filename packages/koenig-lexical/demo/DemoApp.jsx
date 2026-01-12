159a1040516870afbbda3b103b9533d562df3256
Steve Larson <9larsons@gmail.com>
2026-01-12 16:34:40 -0600

    Added email editor to demo

--- packages/koenig-lexical/demo/DemoApp.jsx
+++ packages/koenig-lexical/demo/DemoApp.jsx
@@ -4,21 +4,22 @@ import FloatingButton from './components/FloatingButton';
 import InitialContentToggle from './components/InitialContentToggle';
 import LockIcon from './assets/icons/kg-lock.svg?react';
 import React, {useState} from 'react';
+import ReplacementStringsPlugin from '../src/plugins/ReplacementStringsPlugin';
 import Sidebar from './components/Sidebar';
 import TitleTextBox from './components/TitleTextBox';
 import Watermark from './components/Watermark';
 import WordCount from './components/WordCount';
 import basicContent from './content/basic-content.json';
 import content from './content/content.json';
+import emailContent from './content/email-content.json';
 import minimalContent from './content/minimal-content.json';
 import {$getRoot, $isDecoratorNode} from 'lexical';
 import {
-    BASIC_NODES, BASIC_TRANSFORMERS, KoenigComposableEditor,
-    KoenigComposer, KoenigEditor, MINIMAL_NODES, MINIMAL_TRANSFORMERS,
-    RestrictContentPlugin,
-    TKCountPlugin,
-    WordCountPlugin
+    BASIC_NODES, BASIC_TRANSFORMERS, EMAIL_TEMPLATE_NODES, EMAIL_TRANSFORMERS,
+    KoenigComposableEditor, KoenigComposer, KoenigEditor, MINIMAL_NODES,
+    MINIMAL_TRANSFORMERS, RestrictContentPlugin, TKCountPlugin, WordCountPlugin
 } from '../src';
+import {ListPlugin} from '@lexical/react/LexicalListPlugin';
 import {defaultHeaders as defaultUnsplashHeaders} from './utils/unsplashConfig';
 import {fetchEmbed} from './utils/fetchEmbed';
 import {fileTypes, useFileUpload} from './utils/useFileUpload';
@@ -116,6 +117,8 @@ function getDefaultContent({editorType}) {
         return basicContent;
     } else if (editorType === 'minimal') {
         return minimalContent;
+    } else if (editorType === 'email') {
+        return emailContent;
     }
     return content;
 }
@@ -125,6 +128,8 @@ function getAllowedNodes({editorType}) {
         return BASIC_NODES;
     } else if (editorType === 'minimal') {
         return MINIMAL_NODES;
+    } else if (editorType === 'email') {
+        return EMAIL_TEMPLATE_NODES;
     }
     return undefined;
 }
@@ -149,6 +154,18 @@ function DemoEditor({editorType, registerAPI, cursorDidExitAtTop, darkMode, setW
                 registerAPI={registerAPI}
             >
                 <RestrictContentPlugin paragraphs={1} />
+                <WordCountPlugin onChange={setWordCount} />
+            </KoenigComposableEditor>
+        );
+    } else if (editorType === 'email') {
+        return (
+            <KoenigComposableEditor
+                cursorDidExitAtTop={cursorDidExitAtTop}
+                markdownTransformers={EMAIL_TRANSFORMERS}
+                registerAPI={registerAPI}
+            >
+                <ListPlugin />
+                <ReplacementStringsPlugin />
                 <WordCountPlugin onChange={setWordCount} />
             </KoenigComposableEditor>
         );
@@ -304,7 +321,7 @@ function DemoComposer({editorType, isMultiplayer, setWordCount, setTKCount}) {
         };
     }, [editorAPI]);
 
-    const showTitle = !isMultiplayer && !['basic', 'minimal'].includes(editorType);
+    const showTitle = !isMultiplayer && !['basic', 'minimal', 'email'].includes(editorType);
 
     const cardConfig = {
         ...defaultCardConfig,
@@ -331,7 +348,7 @@ function DemoComposer({editorType, isMultiplayer, setWordCount, setTKCount}) {
             enableMultiplayer={isMultiplayer}
             fileUploader={{useFileUpload: useFileUpload({isMultiplayer}), fileTypes}}
             initialEditorState={initialContent}
-            isTKEnabled={true} // TODO: can we move this onto <KoenigEditor>?
+            isTKEnabled={editorType !== 'email'}
             multiplayerDocId={`demo/${WEBSOCKET_ID}`}
             multiplayerEndpoint={WEBSOCKET_ENDPOINT}
             nodes={getAllowedNodes({editorType})}
