a4828baf6aa418b56bd0fe32973f5aa0ba6b49f5
Steve Larson <9larsons@gmail.com>
2026-01-26 16:55:50 -0600

    Added transistor plugin

---
+++ packages/koenig-lexical/src/plugins/TransistorPlugin.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {$createTransistorNode, INSERT_TRANSISTOR_COMMAND, TransistorNode} from '../nodes/TransistorNode';
+import {COMMAND_PRIORITY_LOW} from 'lexical';
+import {INSERT_CARD_COMMAND} from './KoenigBehaviourPlugin';
+import {mergeRegister} from '@lexical/utils';
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
+
+export const TransistorPlugin = () => {
+    const [editor] = useLexicalComposerContext();
+
+    React.useEffect(() => {
+        if (!editor.hasNodes([TransistorNode])) {
+            console.error('TransistorPlugin: TransistorNode not registered'); // eslint-disable-line no-console
+            return;
+        }
+        return mergeRegister(
+            editor.registerCommand(
+                INSERT_TRANSISTOR_COMMAND,
+                async (dataset) => {
+                    const cardNode = $createTransistorNode(dataset);
+                    editor.dispatchCommand(INSERT_CARD_COMMAND, {cardNode, openInEditMode: true});
+
+                    return true;
+                },
+                COMMAND_PRIORITY_LOW
+            )
+        );
+    }, [editor]);
+
+    return null;
+};
+
+export default TransistorPlugin;
