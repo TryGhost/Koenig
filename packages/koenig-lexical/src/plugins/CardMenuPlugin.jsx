249e95631f3023f453110064d5bd164ae137ec1c
Bora Ata Türkoğlu <bora@BoraDev.local>
2026-02-08 02:54:44 +0300

    Hide mobile plus menu and show slash hint in placeholder

--- packages/koenig-lexical/src/plugins/CardMenuPlugin.jsx
+++ packages/koenig-lexical/src/plugins/CardMenuPlugin.jsx
@@ -2,11 +2,34 @@ import PlusCardMenuPlugin from '../plugins/PlusCardMenuPlugin';
 import React from 'react';
 import SlashCardMenuPlugin from '../plugins/SlashCardMenuPlugin';
 
+function isMobileViewport() {
+    if (typeof window === 'undefined') {
+        return false;
+    }
+
+    return window.innerWidth < 768 && window.innerHeight > window.innerWidth;
+}
+
 export const CardMenuPlugin = () => {
+    const [hidePlusMenu, setHidePlusMenu] = React.useState(false);
+
+    React.useEffect(() => {
+        const updateViewportState = () => {
+            setHidePlusMenu(isMobileViewport());
+        };
+
+        updateViewportState();
+        window.addEventListener('resize', updateViewportState);
+
+        return () => {
+            window.removeEventListener('resize', updateViewportState);
+        };
+    }, []);
+
     return (
         <>
             {/* Koenig Plugins */}
-            <PlusCardMenuPlugin />
+            {!hidePlusMenu && <PlusCardMenuPlugin />}
             <SlashCardMenuPlugin />
         </>
     );
