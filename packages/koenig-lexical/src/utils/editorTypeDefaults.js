9d901db3d019cec8138cad343af844a391362681
Steve Larson <9larsons@gmail.com>
2026-03-03 09:52:55 -0600

    Added types

--- packages/koenig-lexical/src/utils/editorTypeDefaults.js
+++ packages/koenig-lexical/src/utils/editorTypeDefaults.js
@@ -1,5 +1,23 @@
 import {VISIBILITY_SETTINGS} from './visibility';
 
+/**
+ * Currenntly only email is supported.
+ * @typedef {'email'} EditorType
+ */
+
+/**
+ * @typedef {Object} ImageConfig
+ * @property {('regular' | 'wide' | 'full')[]} [allowedWidths]
+ */
+
+/**
+ * @typedef {Object} EditorTypeDefaults
+ * @property {string} [visibilitySettings] - Controls which visibility panels (web/email) are shown
+ * @property {ImageConfig} [image] - Image card configuration
+ * @property {boolean} [isTKEnabled] - Whether TK highlighting is enabled
+ */
+
+/** @type {Partial<Record<EditorType, EditorTypeDefaults>>} */
 const EDITOR_TYPE_DEFAULTS = {
     email: {
         visibilitySettings: VISIBILITY_SETTINGS.EMAIL_ONLY,
@@ -10,6 +28,13 @@ const EDITOR_TYPE_DEFAULTS = {
     }
 };
 
+/**
+ * Returns default cardConfig values for the given editor type.
+ * These are applied in KoenigComposer and can be overridden by explicit cardConfig values.
+ *
+ * @param {EditorType} editorType
+ * @returns {EditorTypeDefaults}
+ */
 export function getEditorTypeDefaults(editorType) {
     return EDITOR_TYPE_DEFAULTS[editorType] || {};
 }
