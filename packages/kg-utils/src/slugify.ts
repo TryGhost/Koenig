03fa69cce6f7ba085a17c4f789ee42fcf88b0d02
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-03-09 07:31:15 +0000

    Migrate kg-utils to TypeScript
    - Move lib/ to src/, rename .js to .ts
    - Add tsconfig.json (strict, NodeNext, ESM)
    - Add "type": "module" to package.json
    - Convert source and tests from CJS to ESM
    - Add type annotations throughout
    - Replace .eslintrc.js with eslint.config.js (flat config)
    - Output to build/ via tsc

--- packages/kg-utils/src/slugify.ts
+++ packages/kg-utils/src/slugify.ts
@@ -1,18 +1,21 @@
-const semver = require('semver');
+import semver from 'semver';
 
-module.exports = function (inputString = '', {ghostVersion = '4.0', type = 'mobiledoc'} = {}) {
+interface SlugifyOptions {
+    ghostVersion?: string;
+    type?: string;
+}
+
+export default function slugify(inputString: unknown = '', {ghostVersion = '4.0', type = 'mobiledoc'}: SlugifyOptions = {}): string {
     const version = semver.coerce(ghostVersion);
 
     if (typeof inputString !== 'string' || (inputString || '').trim() === '') {
         return '';
     }
 
-    if (semver.satisfies(version, '<4.x')) {
+    if (version && semver.satisfies(version, '<4.x')) {
         if (type === 'markdown') {
-            // backwards compatible slugs used in Ghost 0.x to 3.x markdown
             return inputString.replace(/[^\w]/g, '').toLowerCase();
         } else {
-            // backwards compatible slugs used in Ghost 2.x to 3.x mobiledoc
             return inputString.replace(/[<>&"?]/g, '')
                 .trim()
                 .replace(/[^\w]/g, '-')
@@ -20,9 +23,6 @@ module.exports = function (inputString = '', {ghostVersion = '4.0', type = 'mobi
                 .toLowerCase();
         }
     } else {
-        // new slugs introduced in 4.0
-        // allows all chars except symbols but will urlEncode everything
-        // produces %-encoded chars in src but browsers show real chars in status bar and url bar
         return encodeURIComponent(inputString.trim()
             .toLowerCase()
             .replace(/[\][!"#$%&'()*+,./:;<=>?@\\^_{|}~]/g, '')
@@ -30,4 +30,4 @@ module.exports = function (inputString = '', {ghostVersion = '4.0', type = 'mobi
             .replace(/^-|-{2,}|-$/g, '')
         );
     }
-};
+}
